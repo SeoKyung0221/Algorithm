66f15c6f1ac1b13702062739e8187b4616111c97
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2023-07-21 01:40:08 +0900

    [Gold V] Title: 트리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.cc
+++ 백준/Gold/1068. 트리/트리.cc
@@ -8,23 +8,22 @@ int dfs(int here){
 	int child = 0;
 	for(int i : adj[here]){
 		if(i == num) continue;
-		child++;
 		ret += dfs(i);
+		child++;
 	}
 	if(child == 0) return 1;
 	return ret;
 }
 int main(){
 	cin >> n;
+	
 	for(int i = 0; i < n; i++){
 		cin >> num;
 		if(num == -1) root = i;
 		else adj[num].push_back(i);
 	}
+	
 	cin >> num;
-	if(num == root){
-		cout << 0 << "\n";
-		return 0;
-	}
-	cout << dfs(root) << "\n";
+	if(num == root) cout << 0 << "\n";
+	else cout << dfs(root) << "\n";
 }
