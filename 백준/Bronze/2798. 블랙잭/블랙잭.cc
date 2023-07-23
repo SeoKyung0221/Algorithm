57cec4b9b84ba3c553c424f02bdf0cc65a1629ba
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2023-07-23 21:27:17 +0900

    [Bronze II] Title: 블랙잭, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.cc
+++ 백준/Bronze/2798. 블랙잭/블랙잭.cc
@@ -1,32 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 int n, m, ret, a[104];
-vector<int> b;
-
-void combi(int start, vector<int> b){
-	if(b.size() == 3){
-		int cnt = 0;
-		for(int i : b){
-			cnt += i;
-		}
-		if(cnt <= m){
-			ret = max(ret, cnt);
-		}
-		return;
-	}
-	for(int i = start+1; i < n; i++){
-		b.push_back(a[i]);
-		combi(i, b);
-		b.pop_back();
-	}
-	return;
-}
 int main(){
 	cin >> n >> m;
 	for(int i = 0; i < n; i++){
 		cin >> a[i];
 	} 
-	combi(-1, b);
-	
+	for(int i = 0; i < n; i++){
+		for(int j = i+1; j < n; j++){
+			for(int k = j+1; k < n; k++){
+				int cnt = a[i] + a[j] + a[k];
+				if(cnt <= m){
+					ret = max(ret, cnt);
+				}
+			}
+		}
+	}
 	cout << ret << "\n";
 }
