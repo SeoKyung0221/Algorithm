d149f96ed5c62af0005f421fe43d874c733e1684
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2023-07-24 21:05:47 +0900

    [Silver III] Title: 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1072. 게임/게임.cc
+++ 백준/Silver/1072. 게임/게임.cc
@@ -1,22 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-const ll MAX_N = 1e9;
-ll x, y, z, ret = -1;
+ll x, y, ret = -1;
+
 int main(){
 	cin >> x >> y;
-	z = y * 100 / x;
-	
-	ll l = 1, h = MAX_N;
+	ll z = y * 100 / x;
+	ll l = 1; ll h = 1e9;
 	while(l <= h){
 		ll mid = (l+h) / 2;
-		if(z < ((y + mid) * 100 / (x+mid))){
-			ret = mid;
+		if(z < (y+mid) * 100 / (x+mid)){
 			h = mid - 1;
-		}
-		else{
-			l = mid + 1;
-		}
+			ret = mid;
+		}else l = mid + 1;
 	}
- 	cout << ret << "\n";
+	cout << ret << "\n";
 }
