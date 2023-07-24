895305e71f9e0ec251435482c3714e8fe3354309
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2023-07-24 21:21:15 +0900

    [Gold II] Title: 게임, Time: 0 ms, Memory: 2052 KB -BaekjoonHub

--- 백준/Gold/1103. 게임/게임.cc
+++ 백준/Gold/1103. 게임/게임.cc
@@ -1,26 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n, m, dp[54][54], visited[54][54];
-char a[54][54];
+int n, m, visited[54][54], dp[54][54];
 string s;
+char a[54][54];
 const int dy[] = {-1, 0, 1, 0};
 const int dx[] = {0, 1, 0, -1};
 
 int solve(int y, int x, char value){
-	if(value == 'H' || y < 0 || y >= n || x < 0 || x >= m) return 0;
-	
+	if(y < 0 || y >= n || x < 0 || x >= m || value == 'H') return 0;
 	if(visited[y][x] == 1){
 		cout << "-1\n";
 		exit(0);
 	}
 	int &ret = dp[y][x];
 	if(ret != 0) return ret;
-	
+
+	int v = value - '0';
 	visited[y][x] = 1;
-	
 	for(int i = 0; i < 4; i++){
-		int ny = y + (value - '0') * dy[i];
-		int nx = x + (value - '0') * dx[i];
+		int ny = y + v * dy[i];
+		int nx = x + v * dx[i];
 		ret = max(ret, solve(ny, nx, a[ny][nx]) + 1);
 	}
 	visited[y][x] = 0;
