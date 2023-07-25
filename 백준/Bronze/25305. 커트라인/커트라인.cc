f84df32fe94e37bfbd66f0e07be86ac2d9d0c236
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2023-07-25 20:33:24 +0900

    [Bronze II] Title: 커트라인, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.cc
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n, k, num;
+vector<int> v;
+
+int main(){
+    cin >> n >> k;
+    for(int i = 0; i < n; i++){
+        cin >> num;
+        v.push_back(num);
+    }
+    sort(v.begin(), v.end(), greater<int>());
+    cout << v[k-1] << "\n";
+}
