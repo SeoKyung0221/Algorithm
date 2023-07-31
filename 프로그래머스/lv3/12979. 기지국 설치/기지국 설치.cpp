0b59e736b560c905af04dd7a9e7d39f0ba78a06e
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2023-07-31 22:52:22 +0900

    [level 3] Title: 기지국 설치, Time: 0.07 ms, Memory: 4.02 MB -BaekjoonHub

--- 프로그래머스/lv3/12979. 기지국 설치/기지국 설치.cpp
+++ 프로그래머스/lv3/12979. 기지국 설치/기지국 설치.cpp
@@ -1,15 +1,16 @@
-#include <bits/stdc++.h>
-using namespace std; 
-int solution(int n, vector<int> stations, int w){ 
+#include<bits/stdc++.h>
+using namespace std;
+
+int solution(int n, vector<int> stations, int w)
+{
     int ret = 0;
-    int prev = -w;
-    for(int here : stations){
-        int a = prev + w;
-        int b = here - w;
-        int h = b - a - 1;
-        ret += int(ceil((double)h / (double)(2 * w + 1)));
-        prev = here;
+    int prev = 0;
+    for(int i : stations){
+        int a = i - prev - 1 - w;
+        int b = (int)(ceil((double)a / (double)(2 * w + 1)));
+        ret += max(0, b);
+        prev = i + w;
     }
-    ret += int(ceil((double)(n-prev-w) / (double)(2 * w + 1)));
+    ret += (int)(ceil((double)(n - prev) /(double)(2 * w + 1)));
     return ret;
 }
