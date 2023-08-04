4a931d160e8c39cacd45a51af1345936c04a7eae
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2023-08-04 21:08:48 +0900

    [Platinum III] Title: RPG, Time: 468 ms, Memory: 5960 KB -BaekjoonHub

--- 백준/Platinum/1315. RPG/RPG.cc
+++ 백준/Platinum/1315. RPG/RPG.cc
@@ -1,47 +1,46 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n, a, b, c, dp[1004][1004], visited[54], ret;
-struct Q{
-    int x;
-    int y;
-    int z;
+int n, a, b, c, dp[1004][1004], visited[54];
+struct A{
+	int x;
+	int y;
+	int z;
 };
-vector<Q> Quest;
+vector<A> v;
 
-int rpg(int n_str, int n_int){
-    int &ret = dp[n_str][n_int];
-    if(ret != -1) return ret;
-    
-    ret = 0;
-    int point = 0;
-    vector<int> v;
-    for(int i = 0; i < n; i++){
-        if(n_str >= Quest[i].x || n_int >= Quest[i].y){
-            ret++;
-            if(!visited[i]){
-                visited[i] = 1;
-                v.push_back(i);
-                point += Quest[i].z;
-            }
-        }
-    }
-    for(int i = 0; i <= point; i++){
-        int next_str = min(1000, n_str + i);
-        int next_int = min(1000, n_int + point - i);
-        ret = max(ret, rpg(next_str, next_int));
-    }
-    for(int i : v){
-        visited[i] = 0;
-    }
-    return ret;
+int solve(int STR, int INT){
+	int &ret = dp[STR][INT];
+	if(ret != -1) return ret;
+	
+	ret = 0;
+	int point = 0;
+	vector<int> vv;
+	for(int i = 0; i < n; i++){
+		if(v[i].x <= STR || v[i].y <= INT){
+			ret++;
+			if(visited[i] != 1){
+				point += v[i].z;
+				visited[i] = 1;
+				vv.push_back(i);
+			}
+		}
+	}
+	for(int i = 0; i <= point; i++){
+		int STR_N = min(1000, STR+i);
+		int INT_N = min(1000, INT+point - i);
+		ret = max(ret, solve(STR_N, INT_N));
+	}
+	for(int i : vv){
+		visited[i] = 0;
+	}
+	return ret;
 }
 int main(){
-    memset(dp, -1, sizeof(dp));
-    cin >> n;
-    for(int i = 0; i < n; i++){
-        cin >> a >> b >> c;
-        Quest.push_back({a, b, c});
-    }
-    cout << rpg(1,1) << "\n";
-    return 0;
+	memset(dp, -1, sizeof(dp));
+	cin >> n;
+	for(int i = 0; i < n; i++){
+		cin >> a >> b >> c;
+		v.push_back({a, b, c});
+	}
+	cout << solve(1, 1) << "\n";
 }
