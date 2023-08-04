4bbef5a8712612cf6cc3b0315cc4d5a64d123824
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2023-08-04 22:21:44 +0900

    [Gold I] Title: 보석 모으기, Time: 116 ms, Memory: 18916 KB -BaekjoonHub

---
+++ 백준/Gold/1480. 보석 모으기/보석 모으기.cc
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n, m, c, a[17], dp[11][1 << 14][24];
+
+int solve(int idx, int j, int w){
+	if(idx == m) return 0;
+	
+	int &ret = dp[idx][j][w];
+	if(ret != 0) return ret;
+	
+	ret = max(ret, solve(idx+1, j, c));
+	for(int i = 0; i < n; i++){
+		int flag = 1, flag2 = 0;
+		if((1 << i) & j) flag = 0;
+		if(a[i] <= w) flag2 = 1;
+		if(flag && flag2) ret = max(ret, solve(idx, (1 << i) | j, w - a[i]) + 1);
+	}
+	return ret;
+}
+int main(){
+	cin >> n >> m >> c;
+	for(int i = 0; i < n; i++){
+		cin >> a[i];
+	}
+	cout << solve(0, 0, c) << "\n";
+}
