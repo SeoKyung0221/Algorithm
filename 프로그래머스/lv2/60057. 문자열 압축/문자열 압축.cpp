f0d863d86c6235c6a8f9c34e3fbf648164bd204c
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2023-08-06 17:28:23 +0900

    [level 2] Title: 문자열 압축, Time: 0.02 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.cpp
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.cpp
@@ -3,36 +3,33 @@ using namespace std;
 
 int solution(string s) {
     int ret = s.size();
-    for(int i = 1; i <= s.size()/2; i++){
-        int _ret = s.size();
+    for(int i = 1; i <= s.size() / 2; i++){
         vector<string> v;
         int j = 0;
         while(true){
             if(j + i > s.size()){
-                v.push_back(s.substr(j, i));
+                v.push_back(s.substr(j, s.size() - j));
                 break;
             }else{
                 v.push_back(s.substr(j, i));
-                j += i;
             }
+            j += i;
         }
-        string a = v[0];
+        int ret_ = s.size();
         int cnt = 1;
-        for(int i = 1; i < v.size(); i++){
-            if(a == v[i]){
+        string prev = v[0];
+        for(int h = 1; h < v.size(); h++){
+            if(prev == v[h]){
                 cnt++;
             }else{
-                if(cnt != 1){
-                    _ret = _ret - ((cnt-1) * a.size()) + to_string(cnt).size();
-                }
-                cnt = 1;
-                a = v[i];
+                if(cnt != 1) ret_ = ret_ - prev.size() * (cnt - 1) + (int)(to_string(cnt).size()); cnt = 1;
+                prev = v[h];
             }
-            if(i == v.size() - 1 && cnt != 1){
-                _ret = _ret - ((cnt-1) * a.size()) + to_string(cnt).size();
+            if(cnt != 1 && h == v.size() - 1){
+                ret_ = ret_ - prev.size() * (cnt - 1) + (int)(to_string(cnt).size());
             }
         }
-        ret = min(ret, _ret);
+        ret = min(ret, ret_);
     }
     return ret;
 }
