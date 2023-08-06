d177bbda3616b2b32d99e42644b17f45542f83cc
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2023-08-06 18:03:55 +0900

    [level 3] Title: 자물쇠와 열쇠, Time: 0.04 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/lv3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.cpp
+++ 프로그래머스/lv3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.cpp
@@ -6,41 +6,41 @@ void _rotate(vector<vector<int>> &key){
     vector<vector<int>> temp(m, vector<int>(m, 0));
     for(int i = 0; i < m; i++){
         for(int j = 0; j < m; j++){
-            temp[i][j] = key[j][m - 1 - i];
+            temp[i][j] = key[j][m-1-i];
         }
     }
     key = temp;
 }
-
 bool solution(vector<vector<int>> key, vector<vector<int>> lock) {
     int n = lock.size();
     int m = key.size();
-    int cnt1 = 0;
+    int cnt = 0;
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
-            if(lock[i][j] == 0) cnt1++;
+            if(lock[i][j] == 0) cnt++;
         }
     }
-    for(int i = 0; i < 4; i++){
-        for(int dy = -19; dy <= 19; dy++){
-            for(int dx = -19; dx <= 19; dx++){
-                
-                int flag = 0, cnt2 = 0;
+    for(int k = 0; k < 4; k++){
+        for(int i = -19; i <= 19; i++){
+            for(int j = -19; j <= 19; j++){
+                int flag = 0, count = 0;
                 for(int y = 0; y < m; y++){
                     for(int x = 0; x < m; x++){
-                        int ny = y + dy;
-                        int nx = x + dx;
-                        if(nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
+                        int ny = y + i;
+                        int nx = x + j;
+                        if(ny < 0 || ny >= n || nx < 0 || nx >= n) continue;
+                        if(lock[ny][nx] == 0 && key[y][x] == 1){
+                            count++;
+                        }
                         if(lock[ny][nx] == 1 && key[y][x] == 1){
                             flag = 1;
                             continue;
                         }
-                        if(lock[ny][nx] == 0 && key[y][x] == 1){
-                            cnt2++;
-                        }
-                    } 
-                } 
-                if(cnt1 == cnt2 && !flag) return true;
+                    }
+                }
+                if(flag != 1 && count == cnt){
+                    return true;
+                }
             }
         }
         _rotate(key);
