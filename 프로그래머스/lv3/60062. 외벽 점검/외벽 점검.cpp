570fe9889223a968e2bb171c65cd7f637c765ac4
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2023-08-08 22:51:47 +0900

    [level 3] Title: 외벽 점검, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/lv3/60062. 외벽 점검/외벽 점검.cpp
+++ 프로그래머스/lv3/60062. 외벽 점검/외벽 점검.cpp
@@ -1,27 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int INF = 987654321;
 
 vector<int> changeStartIndex(vector<int> weak, int start, int n){
     vector<int> weakList;
-    if(start == 0) return weak;
-    else{
-        for(int i = start; i < weak.size(); i++){
-            weakList.push_back(weak[i]);
-        }
-        for(int i = 0; i < start; i++){
-            weakList.push_back(weak[i] + n);
-        }
+    for(int i = start; i < weak.size(); i++){
+        weakList.push_back(weak[i]);
+    }
+    for(int i = 0; i < start; i++){
+        weakList.push_back(weak[i] + n);
     }
     return weakList;
 }
 int solution(int n, vector<int> weak, vector<int> dist) {
-    int ret = INF;
+    int ret = 1e9;
     sort(dist.begin(), dist.end());
     do{
         for(int i = 0; i < weak.size(); i++){
             vector<int> weakList = changeStartIndex(weak, i, n);
-            int idx = 0, flag = 0, curr = weakList[0] + dist[0];
+            int idx = 0, flag = 0, curr = dist[0] + weakList[0];
             for(int j = 1; j < weakList.size(); j++){
                 if(curr < weakList[j]){
                     if(idx + 1 == dist.size()){
@@ -31,9 +27,9 @@ int solution(int n, vector<int> weak, vector<int> dist) {
                     curr = weakList[j] + dist[++idx];
                 }
             }
-            if(flag == 0)ret = min(ret, idx + 1);
+            if(flag != 1) ret = min(ret, idx + 1);
         }
     }while(next_permutation(dist.begin(), dist.end()));
     
-    return (ret == INF) ? -1 : ret;
+    return (ret == 1e9) ? -1 : ret;
 }
