d44da24adbb36c56efad494ab7a57c1a339f4309
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2023-08-08 22:42:14 +0900

    [level 2] Title: 괄호 변환, Time: 0.05 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/lv2/60058. 괄호 변환/괄호 변환.cpp
+++ 프로그래머스/lv2/60058. 괄호 변환/괄호 변환.cpp
@@ -12,26 +12,24 @@ bool check(string s){
 }
 string solution(string p) {
     if(check(p)) return p;
-    int i, cnt1 = 0, cnt2 = 0;
+    int i = 0, cnt1 = 0, cnt2 = 0;
     for(i = 0; i < p.size(); i++){
         if(p[i] == '(') cnt1++;
         else cnt2++;
-        if(cnt1 == cnt2){
-            break;
-        }
+        if(cnt1 == cnt2) break;
     }
-    string u = p.substr(0, i+1);
+    string u = p.substr(0, i + 1);
     string v = p.substr(i+1, p.size() - (i+1));
     if(check(u)){
         return u + solution(v);
     }else{
         string s = "";
-        s += "(";
+        s += '(';
         s += solution(v);
-        s += ")";
+        s += ')';
         u = u.substr(1, u.size() - 2);
-        for(int j : u){
-            if(j == '(') s += ')';
+        for(int j = 0; j < u.size(); j++){
+            if(u[j] == '(') s += ')';
             else s += '(';
         }
         return s;
