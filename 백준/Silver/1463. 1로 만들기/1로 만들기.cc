4cd31346e279c59971e607aa3b24b81eeb52019a
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2023-08-17 22:42:12 +0900

    [Silver III] Title: 1로 만들기, Time: 36 ms, Memory: 37056 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,22 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
-int x, dp[1000004];
+int n, dp[1000004];
 
-int go(int idx){
-	if(idx == 1) return 0;
+int go(int x){
+	if(x == 1) return 0;
 	
-	int &ret = dp[idx];
+	int &ret = dp[x];
 	if(ret != 1e9) return ret;
 	
-	ret = go(idx-1) + 1;
-	if(idx % 2 == 0) ret = min(ret, go(idx / 2) + 1);
-	if(idx % 3 == 0) ret = min(ret, go(idx / 3) + 1);
+	ret = go(x - 1) + 1;
+	if(x % 2 == 0) ret = min(ret, go(x / 2) + 1);
+	if(x % 3 == 0) ret = min(ret, go(x / 3) + 1);
 	
 	return ret;
 }
 int main(){
 	fill(dp, dp + 1000004, 1e9);
-	cin >> x;
+	cin >> n;
 	
-	cout << go(x) << "\n";
+	cout << go(n) << "\n";
 }
