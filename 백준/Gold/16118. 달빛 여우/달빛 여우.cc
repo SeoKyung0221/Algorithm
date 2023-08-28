892451bb74678487f296ffe25170956e97f8dd57
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2023-08-28 13:02:49 +0900

    [Gold I] Title: 달빛 여우, Time: 76 ms, Memory: 10420 KB -BaekjoonHub

--- 백준/Gold/16118. 달빛 여우/달빛 여우.cc
+++ 백준/Gold/16118. 달빛 여우/달빛 여우.cc
@@ -1,30 +1,30 @@
 #include<bits/stdc++.h>
-using namespace std; 
-typedef pair<int, int> PI; 
-typedef tuple<int,int,bool>TI; 
-int n, m, a, b, d, ret, here, cnt, dist[4001], dist_wolf[4001][2]; 
-vector<PI> adj[4001];   
-const int INF = 987654321;
-void dikstra(){ 
-    fill(dist, dist + 4001, INF);
-    priority_queue<PI, vector<PI>, greater<PI>> PQ; 
-    dist[1] = 0; 
-    PQ.push(make_pair(0, 1));
-    while(PQ.size()){ 
-        int here = PQ.top().second; 
-        int here_dist = PQ.top().first; 
-        PQ.pop();   
+using namespace std;
+typedef tuple<int, int, bool> TI;
+int n, m, a, b, d, dist[4004], dist_wolf[4004][2], ret;
+const int INF = 1e9;
+vector<pair<int,int>> adj[4004];
+
+void dikstra(){
+	fill(dist, dist + 4004, INF);
+	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+	dist[1] = 0;
+	pq.push({0, 1});
+	while(pq.size()){
+		int here = pq.top().second;
+		int here_dist = pq.top().first;
+		pq.pop();
 		if(dist[here] != here_dist) continue;
-        for(PI there : adj[here]){
-            int next = there.second; 
-            int d = there.first; 
-            if(dist[next] > dist[here] + d){
-                dist[next] = dist[here] + d; 
-                PQ.push(make_pair(dist[next], next));
-            }
-        }
-    }
-}  
+		for(auto i : adj[here]){
+			int next = i.second;
+			int next_dist = i.first;
+			if(dist[next] > dist[here] + next_dist){
+				dist[next] = dist[here] + next_dist;
+				pq.push({dist[next], next});
+			}
+		}
+	}
+}
 void dikstra2(){ 
     fill(&dist_wolf[0][0], &dist_wolf[0][0] + 4001 * 2, INF);
     priority_queue<TI, vector<TI>, greater<TI>> PQ; 
@@ -35,7 +35,7 @@ void dikstra2(){
         tie(d, here, cnt) = PQ.top();
         PQ.pop();    
 		if(dist_wolf[here][cnt] != d) continue;
-        for(PI there : adj[here]){
+        for(auto there : adj[here]){
             int next = there.second; 
             int d = !cnt ? there.first / 2 : there.first * 2;    
             if(dist_wolf[next][!cnt] > dist_wolf[here][cnt] + d){ 
@@ -45,20 +45,22 @@ void dikstra2(){
         }
     }       
 } 
-
-
-int main(){ 
-    scanf("%d %d", &n, &m);
-    for(int i = 0; i < m; i++){
-        scanf("%d %d %d", &a, &b, &d); 
-        adj[a].push_back(make_pair(d * 2, b)); 
-        adj[b].push_back(make_pair(d * 2, a)); 
-    } 
-    dikstra();  
-    dikstra2();  
-    for(int i = 2; i <= n; i++){   
-        int d = min(dist_wolf[i][0], dist_wolf[i][1]); 
-        if(d > dist[i]) ret++;  
-    }
-    printf("%d\n", ret);
-} 
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	cin >> n >> m;
+	for(int i = 0; i < m; i++){
+		cin >> a >> b >> d;
+		adj[a].push_back({2 * d, b});
+		adj[b].push_back({2 * d, a});
+	}
+	dikstra();
+	dikstra2();
+	
+	for(int i = 2; i <= n; i++){
+		int k = min(dist_wolf[i][0], dist_wolf[i][1]);
+		if(k > dist[i]) ret++;
+	}
+	cout << ret << "\n";
+}
