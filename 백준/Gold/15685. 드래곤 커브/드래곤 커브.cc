c3b0b9723a383881aa7b52007111e3fe7cccf774
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2023-09-03 14:11:04 +0900

    [Gold IV] Title: 드래곤 커브, Time: 0 ms, Memory: 2064 KB -BaekjoonHub

---
+++ 백준/Gold/15685. 드래곤 커브/드래곤 커브.cc
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int dy[] = {0, -1, 0, 1};
+const int dx[] = {1, 0, -1, 0};
+int n, x, y, d, g, a[104][104], ret;
+vector<int> dragon[4][11];
+
+void makeDragon(){
+	for(int i = 0; i < 4; i++){
+		dragon[i][0].push_back(i);
+		dragon[i][1].push_back((i + 1) % 4);
+		for(int j = 2; j <= 10; j++){
+			int n = dragon[i][j-1].size();
+			for(int k = n - 1; k >= 0; k--){
+				dragon[i][j].push_back((dragon[i][j-1][k] + 1) % 4);
+			}
+			for(int k = 0; k < n; k++){
+				dragon[i][j].push_back(dragon[i][j-1][k]);
+			}
+		}
+	}
+	return;
+}
+
+void go(int x, int y, int d, int g){
+	a[y][x] = 1;
+	for(int i = 0; i <= g; i++){
+		for(int j : dragon[d][i]){
+			x += dx[j];
+			y += dy[j];
+			a[y][x] = 1;
+		}
+	}
+	return;
+}
+int main(){
+	cin >> n;
+	makeDragon();
+	for(int i = 0; i < n; i++){
+		cin >> x >> y >> d >> g;
+		go(x, y, d, g);
+	}
+	for(int i = 0; i <= 100; i++){
+		for(int j = 0; j <= 100; j++){
+			if(a[i][j] && a[i][j+1] && a[i+1][j] && a[i+1][j+1]){
+				ret++;
+			}
+		}
+	}
+	cout << ret << "\n";
+}
