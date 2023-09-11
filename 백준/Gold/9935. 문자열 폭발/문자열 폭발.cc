3d304750123deea636a38f9ea61687dff18e5c33
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2023-09-11 23:42:25 +0900

    [Gold IV] Title: 문자열 폭발, Time: 88 ms, Memory: 7796 KB -BaekjoonHub

---
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+string s, t, ret;
+
+int main(){
+	cin >> s >> t;
+	for(char a : s){
+		ret += a;
+		if(ret.size() >= t.size() && ret.substr(ret.size() - t.size(), t.size()) == t){
+			ret.erase(ret.end() - t.size(), ret.end());
+		}
+	}
+	if(ret.size()){
+		cout << ret << "\n";
+	}else{
+		cout << "FRULA" << "\n";
+	}
+	return 0;
+}
