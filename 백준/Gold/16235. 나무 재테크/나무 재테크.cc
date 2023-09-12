28a42ab2fce7733d9faffe49874ce853395d2d59
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2023-09-12 18:53:51 +0900

    [Gold III] Title: 나무 재테크, Time: 152 ms, Memory: 2292 KB -BaekjoonHub

--- 백준/Gold/16235. 나무 재테크/나무 재테크.cc
+++ 백준/Gold/16235. 나무 재테크/나무 재테크.cc
@@ -1,37 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int dy[] = {-1, -1, -1, 0, 0, 1, 1, 1};
-const int dx[] = {-1, 0, 1, -1, 1, -1, 0, 1};
 int n, m, k, a[11][11], b[11][11], x, y, z, ret;
 vector<int> tree[11][11];
+const int dy[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+const int dx[] = {-1, 0, 1, -1, 1, -1, 0, 1};
 
-void springSummer(){
+void SpringSummer(){
 	for(int i = 0; i < n; i++){
 		for(int j = 0; j < n; j++){
 			sort(tree[i][j].begin(), tree[i][j].end());
 			vector<int> temp;
-			int c = 0;
-			for(int k = 0; k < tree[i][j].size(); k++){
-				if(tree[i][j][k] <= a[i][j]){
-					a[i][j] -= tree[i][j][k];
-					temp.push_back(tree[i][j][k] + 1);
+			int c = 0;		
+			for(int k : tree[i][j]){
+				if(k <= b[i][j]){
+					temp.push_back(k+1);
+					b[i][j] -= k;
 				}else{
-					c += tree[i][j][k] / 2;
+					c += k / 2;
 				}
 			}
 			tree[i][j] = temp;
-			a[i][j] += c;
+			b[i][j] += c;
 		}
 	}
 }
 void fall(){
 	for(int i = 0; i < n; i++){
 		for(int j = 0; j < n; j++){
-			for(int t: tree[i][j]){
-				if(t % 5 == 0){
-					for(int k = 0; k < 8; k++){
-						int ny = i + dy[k];
-						int nx = j + dx[k];
+			for(int k : tree[i][j]){
+				if(k % 5 == 0){
+					for(int h = 0; h < 8; h++){
+						int ny = i + dy[h];
+						int nx = j + dx[h];
 						if(ny < 0 || ny >= n || nx < 0 || nx >= n) continue;
 						tree[ny][nx].push_back(1);
 					}
@@ -43,16 +43,16 @@ void fall(){
 void winter(){
 	for(int i = 0; i < n; i++){
 		for(int j = 0; j < n; j++){
-			a[i][j] += b[i][j];
+			b[i][j] += a[i][j];
 		}
 	}
 }
 int main(){
-	fill(&a[0][0], &a[0][0] + 11 * 11, 5);
+	fill(&b[0][0], &b[0][0] + 11 * 11, 5);
 	cin >> n >> m >> k;
 	for(int i = 0; i < n; i++){
 		for(int j = 0; j < n; j++){
-			cin >> b[i][j];
+			cin >> a[i][j];
 		}
 	}
 	for(int i = 0; i < m; i++){
@@ -60,7 +60,7 @@ int main(){
 		tree[--x][--y].push_back(z);
 	}
 	for(int i = 0; i < k; i++){
-		springSummer();
+		SpringSummer();
 		fall();
 		winter();
 	}
