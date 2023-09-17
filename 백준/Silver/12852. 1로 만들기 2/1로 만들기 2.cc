a7be5c38dea33b6df899c5273b201b8bf248c403
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2023-09-18 00:30:03 +0900

    [Silver I] Title: 1로 만들기 2, Time: 8 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.cc
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.cc
@@ -7,7 +7,7 @@ void go(int here){
 	
 	cout << here << " ";
 	
-	if(dp[here] == dp[here - 1] + 1) go(here - 1);
+	if(dp[here] == dp[here-1] + 1) go(here-1);
 	else if(here % 2 == 0 && dp[here] == dp[here / 2] + 1) go(here / 2);
 	else if(here % 3 == 0 && dp[here] == dp[here / 3] + 1) go(here / 3);
 	return;
@@ -16,7 +16,7 @@ int main(){
 	fill(dp, dp + 1000004, 1e9);
 	cin >> n;
 	dp[1] = 0;
-	for(int i = 2; i <= n; i++){
+	for(int i = 1; i <= n; i++){
 		dp[i] = min(dp[i], dp[i-1] + 1);
 		if(i % 2 == 0) dp[i] = min(dp[i], dp[i / 2] + 1);
 		if(i % 3 == 0) dp[i] = min(dp[i], dp[i / 3] + 1);
