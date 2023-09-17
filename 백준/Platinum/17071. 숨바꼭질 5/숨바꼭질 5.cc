537416b5f4803373f5b7af5c59343c2190243736
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2023-09-18 00:14:25 +0900

    [Platinum V] Title: 숨바꼭질 5, Time: 16 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Platinum/17071. 숨바꼭질 5/숨바꼭질 5.cc
+++ 백준/Platinum/17071. 숨바꼭질 5/숨바꼭질 5.cc
@@ -1,38 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define kkkk time
-int n, k, ok, turn = 1, visited[2][500004];
+const int MAX_N = 500000;
+int n, k, visited[2][MAX_N + 4], ok, turn = 1;
 
 int main(){
 	cin >> n >> k;
-    if(n == k){
-        cout << 0 << "\n";
-        return 0;
-    }
-	
-	visited[0][n] = 1;
+	if(n == k){
+		cout << 0 << "\n";
+		return 0;
+	}
 	queue<int> q;
+	visited[0][n] = 1;
 	q.push(n);
-	
 	while(q.size()){
 		k += turn;
-		if(k > 500000) break;
+		
+		if(k > MAX_N) break;
 		if(visited[turn % 2][k]){
 			ok = 1;
 			break;
 		}
 		int qSize = q.size();
 		for(int i = 0; i < qSize; i++){
-			int here = q.front(); q.pop();
-			for(int next : {here - 1, here + 1, here * 2}){
-				if(next < 0 || next > 500000) continue;
-				if(visited[turn % 2][next]) continue;
-				visited[turn % 2][next] = 1;
-				if(next == k){
+			int x = q.front(); q.pop();
+			for(int nx : {x-1, x+1, x*2}){
+				if(nx < 0 || nx > MAX_N || visited[turn % 2][nx]) continue;
+				visited[turn % 2][nx] = 1;
+				if(nx == k){
 					ok = 1;
 					break;
 				}
-				q.push(next);
+				q.push(nx);
 			}
 			if(ok) break;
 		}
