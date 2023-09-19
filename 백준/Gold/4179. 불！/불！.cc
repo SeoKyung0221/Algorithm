b799ac85aa47d9ac38b0afca9c0fd929b83fa3c3
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2023-09-19 23:03:09 +0900

    [Gold IV] Title: 불!, Time: 52 ms, Memory: 11016 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.cc
+++ 백준/Gold/4179. 불！/불！.cc
@@ -1,60 +1,61 @@
 #include<bits/stdc++.h>
 using namespace std;
+queue<pair<int, int>> q;
 const int INF = 1e9;
 const int dy[] = {-1, 0, 1, 0};
 const int dx[] = {0, 1, 0, -1};
-int n, m, sy, sx, fire_visited[1004][1004], person_visited[1004][1004], ret;
+int r, c, fire_visited[1004][1004], person_visited[1004][1004], ret;
 char a[1004][1004];
+vector<pair<int,int>> v;
 
 int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
 	fill(&fire_visited[0][0], &fire_visited[0][0] + 1004 * 1004, INF);
-	queue<pair<int, int>> q;
-	cin >> n >> m;
-	for(int i = 0; i < n; i++){
-		for(int j = 0; j < m; j++){
+	cin >> r >> c;
+	for(int i = 0; i < r; i++){
+		for(int j = 0; j < c; j++){
 			cin >> a[i][j];
 			if(a[i][j] == 'J'){
-				sy = i;
-				sx = j;
-				person_visited[sy][sx] = 1;
+				person_visited[i][j] = 1;
+				v.push_back({i, j});
 			}else if(a[i][j] == 'F'){
 				fire_visited[i][j] = 1;
-				q.push({i,j});
+				q.push({i, j});
 			}
 		}
 	}
 	while(q.size()){
 		int y, x;
-		tie(y, x) = q.front(); q.pop();
+		tie(y, x) = q.front();
+		q.pop();
 		for(int i = 0; i < 4; i++){
 			int ny = y + dy[i];
 			int nx = x + dx[i];
-			if(ny < 0 || ny >= n || nx < 0 || nx >= m || fire_visited[ny][nx] != INF || a[ny][nx] == '#') continue;
+			if(ny < 0 || ny >= r || nx < 0 || nx >= c || fire_visited[ny][nx] != INF || a[ny][nx] == '#') continue;
 			fire_visited[ny][nx] = fire_visited[y][x] + 1;
 			q.push({ny,nx});
 		}
 	}
-	q.push({sy, sx});
+	q.push({v[0].first, v[0].second});
 	while(q.size()){
 		int y, x;
-		tie(y, x) = q.front(); q.pop();
-		if(y == 0 || y == n-1 || x == 0 || x == m-1){
+		tie(y, x) = q.front();
+		if(y == 0 || y == r-1 || x == 0 || x == c-1){
 			ret = person_visited[y][x];
 			break;
 		}
+		q.pop();
 		for(int i = 0; i < 4; i++){
 			int ny = y + dy[i];
 			int nx = x + dx[i];
-			if(ny < 0 || ny >= n || nx < 0 || nx >= m || person_visited[ny][nx] || a[ny][nx] == '#') continue;
+			if(ny < 0 || ny >= r || nx < 0 || nx >= c || person_visited[ny][nx] || a[ny][nx] == '#') continue;
 			if(fire_visited[ny][nx] <= person_visited[y][x] + 1) continue;
 			person_visited[ny][nx] = person_visited[y][x] + 1;
-			q.push({ny,nx});
+			q.push({ny, nx});
 		}
 	}
-	if(ret != 0){
-		cout << ret << "\n";
-	}else{
-		cout << "IMPOSSIBLE\n";
-	}
-	return 0;
+	if(ret == 0) cout << "IMPOSSIBLE\n";
+	else cout << ret << "\n";
 }
