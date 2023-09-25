05b6e5da5802a7bcffaf0b164a5406becbf44340
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2023-09-25 13:33:34 +0900

    [Gold IV] Title: 사탕 가게, Time: 248 ms, Memory: 2064 KB -BaekjoonHub

--- 백준/Gold/4781. 사탕 가게/사탕 가게.cc
+++ 백준/Gold/4781. 사탕 가게/사탕 가게.cc
@@ -1,20 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n, c, a, b, d, e, dp[10004];
+int n, m, l, c, p, o, dp[11000];
 
 int main(){
 	while(true){
-		fill(dp, dp + 10004, 0);
-		scanf("%d %d.%d", &n , &a, &b);
-		int p = a * 100 + b;
-		if(n == 0 && a == 0 && b == 00) break;
+		fill(dp, dp + 11000, 0);
+		scanf("%d %d.%d", &n, &m, &l);
+		if(n == 0 && m == 0 && l == 00) break;
+		int a = 100 * m + l;
 		for(int i = 0; i < n; i++){
-			scanf("%d %d.%d", &c, &d, &e);
-			int q = d * 100 + e;
-			for(int j = q; j <= p; j++){
-				dp[j] = max(dp[j], dp[j - q] + c);
+			scanf("%d %d.%d", &c, &p, &o);
+			int b = 100 * p + o;
+			for(int j = b; j <= a; j++){
+				dp[j] = max(dp[j], dp[j-b] + c);
 			}
 		}
-		cout << dp[p] << "\n"; 
+		cout << dp[a] << "\n";
 	}
 }
