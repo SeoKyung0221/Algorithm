910348b9effdd1b260dabc30adbed4018369256d
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2023-10-09 17:47:17 +0900

    [Silver IV] Title: 연속부분최대곱, Time: 4 ms, Memory: 2100 KB -BaekjoonHub

---
+++ 백준/Silver/2670. 연속부분최대곱/연속부분최대곱.cc
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n;
+double a[10004], ret;
+
+int main(){
+	cin >> n;
+	for(int i = 0; i < n; i++){
+		cin >> a[i];
+	}
+	double temp = 1;
+	for(int i = 0; i < n; i++){
+		temp *= a[i];
+		ret = max(ret, temp);
+		if(temp < 1){
+			temp = 1;
+		}
+	}
+	printf("%0.3lf", ret);
+}
