2af655b00b4e25b1d71ce2038eb7c702b6cc71aa
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2023-10-12 19:53:56 +0900

    [Silver II] Title: 용돈 관리, Time: 48 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Silver/6236. 용돈 관리/용돈 관리.cc
+++ 백준/Silver/6236. 용돈 관리/용돈 관리.cc
@@ -1,19 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-ll n, m, a[100004], mx, ret;
+ll n, m, a[100004], ret, mx;
 
 bool check(ll mid){
 	ll temp = 1;
 	ll now = mid;
 	for(int i = 0; i < n; i++){
-		if(a[i] > now){
+		if(now - a[i] < 0){
 			temp++;
 			now = mid;
 			now -= a[i];
-		}else{
-			now -= a[i];
-		}
+		}else now -=a[i];
 	}
 	return temp <= m;
 }
@@ -21,9 +19,9 @@ int main(){
 	cin >> n >> m;
 	for(int i = 0; i < n; i++){
 		cin >> a[i];
-		mx = max(a[i], mx);
+		mx = max(mx, a[i]);
 	}
-	ll l = mx, h = 1e9;
+	ll l = mx, h = 1e9 + 4;
 	while(l <= h){
 		ll mid = (l + h) / 2;
 		if(check(mid)){
