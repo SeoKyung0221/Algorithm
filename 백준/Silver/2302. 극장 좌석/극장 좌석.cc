95874570ef6b0e48c65102fbf85c01f0e9b1869f
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2023-10-13 13:15:18 +0900

    [Silver I] Title: 극장 좌석, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2302. 극장 좌석/극장 좌석.cc
+++ 백준/Silver/2302. 극장 좌석/극장 좌석.cc
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n, m, num, _fix[44], dp[44];
+int n, m, num, a[44], dp[44];
 
 int go(int cnt){
 	if(cnt >= n-1) return 1;
@@ -8,18 +8,17 @@ int go(int cnt){
 	int &ret = dp[cnt];
 	if(ret != 0) return ret;
 	
-	if(_fix[cnt]) ret += go(cnt+1);
-	else if(_fix[cnt+1]) ret += go(cnt + 1);
+	if(a[cnt]) ret += go(cnt+1);
+	else if(a[cnt+1]) ret += go(cnt+1);
 	else ret += go(cnt+1) + go(cnt+2);
 	
 	return ret;
 }
-
 int main(){
 	cin >> n >> m;
 	for(int i = 0; i < m; i++){
 		cin >> num;
-		_fix[num-1] = 1;
+		a[num-1] = 1;
 	}
 	cout << go(0) << "\n";
 }
