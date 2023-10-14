a652ab959bd4a95733a9b83bfdfef9025b4214d4
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2023-10-14 15:15:52 +0900

    [Gold V] Title: 치킨 배달, Time: 4 ms, Memory: 2168 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.cc
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.cc
@@ -1,18 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 int n, m, a[54][54], ret = 1e9;
-vector<pair<int, int>> c, h;
-vector<vector<int>> b;
+vector<pair<int,int>> h, c;
+vector<vector<int>> v;
 
-void combi(int start, vector<int> v){
-	if(v.size() == m){
-		b.push_back(v);
+void combi(int start, vector<int> &b){
+	if(b.size() == m){
+		v.push_back(b);
 		return;
 	}
 	for(int i = start + 1; i < c.size(); i++){
-		v.push_back(i);
-		combi(i, v);
-		v.pop_back();
+		b.push_back(i);
+		combi(i, b);
+		b.pop_back();
 	}
 	return;
 }
@@ -21,18 +21,18 @@ int main(){
 	for(int i = 0; i < n; i++){
 		for(int j = 0; j < n; j++){
 			cin >> a[i][j];
-			if(a[i][j] == 1) h.push_back({i,j});
-			if(a[i][j] == 2) c.push_back({i,j});
+			if(a[i][j] == 1) h.push_back({i, j});
+			else if(a[i][j] == 2) c.push_back({i, j});
 		}
 	}
-	vector<int> v;
-	combi(-1, v);
-	for(vector<int> i : b){
+	vector<int> b;
+	combi(-1, b);
+	for(vector<int> i : v){
 		int sum = 0;
-		for(auto home : h){
+		for(auto j : h){
 			int cur = 1e9;
-			for(int ch: i){
-				cur = min(cur, abs(home.first - c[ch].first) + abs(home.second - c[ch].second));
+			for(int k : i){
+				cur = min(cur, abs(j.first - c[k].first) + abs(j.second - c[k].second));
 			}
 			sum += cur;
 		}
