d3e96f93bbad8651e5e35765e245af89d3410766
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2023-10-18 05:41:27 +0900

    [Gold III] Title: 종이 조각, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/14391. 종이 조각/종이 조각.cc
+++ 백준/Gold/14391. 종이 조각/종이 조각.cc
@@ -24,19 +24,19 @@ int main(){
 			}
 			sum += cur;
 		}
-		for (int j = 0; j < m; j++) {
-            int cur = 0;
-            for (int i = 0; i < n; i++) {
-                int k = i * m + j;
-                if ((s & (1<<k)) != 0) {
-                    cur = cur * 10 + a[i][j];
-                } else {
-                    sum += cur;
-                    cur = 0;
-                }
-            }
-            sum += cur;
-        }
+		for(int j = 0; j < m; j++){
+			int cur = 0;
+			for(int i = 0; i < n; i++){
+				int k = i * m + j;
+				if((s & (1 << k)) != 0){
+					cur = cur * 10 + a[i][j];
+				}else{
+					sum += cur;
+					cur = 0;
+				}
+			}
+			sum += cur;
+		}
 		ret = max(ret, sum);
 	}
 	cout << ret << "\n";
