d8fc4ed102180bc9417de8faa2063250dc8e5bfa
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2023-10-24 17:52:08 +0900

    [Gold IV] Title: 오큰수, Time: 248 ms, Memory: 14112 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n, a[1000004], ret[1000004];
+stack<int> stk;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	memset(ret, -1, sizeof(ret));
+	cin >> n;
+	for(int i = 0; i < n; i++){
+		cin >> a[i];
+		while(stk.size() && a[stk.top()] < a[i]){
+			ret[stk.top()] = a[i];
+			stk.pop();
+		}
+		stk.push(i);
+	}
+	for(int i = 0; i < n; i++){
+		cout << ret[i] << " ";
+	}
+}
