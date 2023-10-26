4f52aeadfaad81bfb47096c6bdedcefa51648c4d
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2023-10-26 21:38:03 +0900

    [Gold IV] Title: 게리맨더링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/17471. 게리맨더링/게리맨더링.cc
+++ 백준/Gold/17471. 게리맨더링/게리맨더링.cc
@@ -1,20 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n, a[11], visited[11], num, b, ret = 1e9, comp[11];
+int n, a[11], visited[11], comp[11], num, b, ret = 1e9;
 vector<int> adj[11];
 
-pair<int, int> dfs(int here, int value){
-    visited[here] = 1; 
-    pair<int, int> ret = {1, a[here]}; 
-    for(int there : adj[here]){
-        if(comp[there] != value) continue; 
-        if(visited[there]) continue; 
-        pair<int, int> _temp = dfs(there, value); 
-        ret.first += _temp.first; 
-        ret.second += _temp.second;  
-    }
-    return ret; 
-}  
+pair<int, int> dfs(int here, int flag){
+	visited[here] = 1;
+	pair<int, int> ret = {1, a[here]};
+	for(int i : adj[here]){
+		if(visited[i]) continue;
+		if(comp[i] != flag) continue;
+		pair<int,int> _temp = dfs(i, flag);
+		ret.first += _temp.first;
+		ret.second += _temp.second;
+	}
+	return ret;
+}
 int main(){
 	cin >> n;
 	for(int i = 1; i <= n; i++){
@@ -25,27 +25,26 @@ int main(){
 		for(int j = 0; j < num; j++){
 			cin >> b;
 			adj[i].push_back(b);
-			
 		}
 	}
 	for(int i = 1; i < (1 << n) - 1; i++){
-		int hereZ = 0, hereO = 0;
-		fill(comp, comp + 11, 0);
 		fill(visited, visited + 11, 0);
+		fill(comp, comp + 11, 0);
+		int hereZ = 0, hereO = 0;
 		for(int j = 0; j < n; j++){
 			if(i & (1 << j)){
-				hereO = j + 1;
 				comp[j+1] = 1;
+				hereO = j+1;
 			}else{
-				hereZ = j + 1;
+				hereZ = j+1;
 			}
-			}
-			pair<int, int> z = dfs(hereZ, 0);
-			pair<int, int> o = dfs(hereO, 1);
-			if(z.first + o.first == n){
-				ret = min(ret, abs(z.second - o.second));
+		}
+		pair<int, int> z = dfs(hereZ, 0);
+		pair<int, int> o = dfs(hereO, 1);
+		if(z.first + o.first == n){
+			ret = min(ret, abs(z.second - o.second));
 		}
 	}
-	if(ret == 1e9) cout << "-1\n";
+	if(ret == 1e9) cout << -1 << "\n";
 	else cout << ret << "\n";
 }
