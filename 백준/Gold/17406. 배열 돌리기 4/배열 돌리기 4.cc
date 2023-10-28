1ca706fd84e755ca7bd5741c770bee4fa6e29b3c
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2023-10-29 02:51:29 +0900

    [Gold IV] Title: 배열 돌리기 4, Time: 72 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/17406. 배열 돌리기 4/배열 돌리기 4.cc
+++ 백준/Gold/17406. 배열 돌리기 4/배열 돌리기 4.cc
@@ -2,45 +2,44 @@
 using namespace std;
 const int dy[] = {0, 1, 0, -1};
 const int dx[] = {1, 0, -1, 0};
-int n, m, k, a[54][54], b[54][54], visited[54][54], r, c, s, sy, sx, ey, ex, dir, ret = 1e9;
+int n, m, k, a[54][54], visited[54][54], b[54][54], r, c, s, sy, sx, ey, ex, dir, ret = 1e9;
 struct A{
 	int y, x, cnt;
 };
 vector<A> v;
 vector<int> v_idx;
-vector<pair<int,int>> vv;
-
-void dfs(int y, int x, int flag){
-	if(!flag && y == sy && x == sx) dir = (dir + 1) % 4;
-	if(!flag && y == sy && x == ex) dir = (dir + 1) % 4;
-	if(!flag && y == ey && x == sx) dir = (dir + 1) % 4;
-	if(!flag && y == ey && x == ex) dir = (dir + 1) % 4;
+vector<pair<int, int>> vv;
+void go(int y, int x, int first){
+	if(!first && y == sy && x == sx) dir++;
+	if(!first && y == sy && x == ex) dir++;
+	if(!first && y == ey && x == ex) dir++;
+	if(!first && y == ey && x == sx) dir++;
 	int ny = y + dy[dir];
 	int nx = x + dx[dir];
 	if(visited[ny][nx]) return;
 	vv.push_back({ny, nx});
 	visited[ny][nx] = 1;
-	dfs(ny, nx, 0);
+	go(ny, nx, 0);
 }
 void rotateAll(int y, int x, int cnt){
-	for(int i = cnt; i >= 1; i--){
+	memset(visited, 0, sizeof(visited));
+	for(int s = 1; s <= cnt; s++){
 		vv.clear();
-		memset(visited, 0, sizeof(visited));
-		sy = y - i;
-		sx = x - i;
-		ey = y + i;
-		ex = x + i;
+		sy = y - s;
+		sx = x - s;
+		ey = y + s;
+		ex = x + s;
 		dir = 0;
-		vv.push_back({sy, sx});
 		visited[sy][sx] = 1;
-		dfs(sy, sx, 1);
+		vv.push_back({sy, sx});
+		go(sy, sx, 1);
 		vector<int> vvv;
-		for(auto j : vv){
-			vvv.push_back(b[j.first][j.second]);
+		for(auto i : vv){
+			vvv.push_back(b[i.first][i.second]);
 		}
 		rotate(vvv.rbegin(), vvv.rbegin() + 1, vvv.rend());
-		for(int j = 0; j < vv.size(); j++){
-			b[vv[j].first][vv[j].second] = vvv[j];
+		for(int i = 0; i < vv.size(); i++){
+			b[vv[i].first][vv[i].second] = vvv[i];
 		}
 	}
 }
@@ -48,15 +47,15 @@ int solve(){
 	for(int i : v_idx){
 		rotateAll(v[i].y, v[i].x, v[i].cnt);
 	}
-	int sum = 1e9;
+	int ans = 1e9;
 	for(int i = 0; i < n; i++){
-		int cur = 0;
+		int sum = 0;
 		for(int j = 0; j < m; j++){
-			cur += b[i][j];
+			sum += b[i][j];
 		}
-		sum = min(sum, cur);
+		ans = min(ans, sum);
 	}
-	return sum;
+	return ans;
 }
 int main(){
 	cin >> n >> m >> k;
@@ -70,6 +69,7 @@ int main(){
 		v.push_back({r, c, s});
 		v_idx.push_back(i);
 	}
+	
 	do{
 		memcpy(b, a, sizeof(a));
 		ret = min(ret, solve());
