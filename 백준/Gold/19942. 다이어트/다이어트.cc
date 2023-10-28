507524797a3e26312b3e7de13280e757b6aeac22
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2023-10-29 02:13:07 +0900

    [Gold IV] Title: 다이어트, Time: 12 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/19942. 다이어트/다이어트.cc
+++ 백준/Gold/19942. 다이어트/다이어트.cc
@@ -1,25 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int INF = 1e9;
-int n, mp, mf, ms, mv, ret = INF;
+int n, mp, mf, ms, mv, ret = 1e9;
 struct A{
 	int p, f, s, v, c;
 };
-
-A a[16];
-map<int, vector<vector<int>>> mpp;
- 
+A a[20];
+map<int, vector<vector<int>>> sto;
 int main(){
 	cin >> n >> mp >> mf >> ms >> mv;
 	for(int i = 0; i < n; i++){
 		cin >> a[i].p >> a[i].f >> a[i].s >> a[i].v >> a[i].c;
 	}
 	for(int i = 0; i < (1 << n); i++){
-		int cp = 0, cf = 0, cs = 0, cv = 0, cc = 0;
 		vector<int> v;
+		int cp = 0, cf = 0, cs = 0, cv = 0, cc = 0;
 		for(int j = 0; j < n; j++){
 			if(i & (1 << j)){
-				v.push_back(j+1);
+				v.push_back(j + 1);
 				cp += a[j].p;
 				cf += a[j].f;
 				cs += a[j].s;
@@ -30,15 +27,15 @@ int main(){
 		if(cp >= mp && cf >= mf && cs >= ms && cv >= mv){
 			if(ret >= cc){
 				ret = cc;
-				mpp[ret].push_back(v);
+				sto[ret].push_back(v);
 			}
 		}
 	}
-	if(ret == INF) cout << -1 << "\n";
+	if(ret == 1e9) cout << "-1";
 	else{
-		sort(mpp[ret].begin(), mpp[ret].end());
 		cout << ret << "\n";
-		for(int i : mpp[ret][0]){
+		sort(sto[ret].begin(), sto[ret].end());
+		for(int i : sto[ret][0]){
 			cout << i << " ";
 		}
 	}
