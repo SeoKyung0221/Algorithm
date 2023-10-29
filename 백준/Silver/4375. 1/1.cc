b582de9d920f43ddc924da47bfcb9bcbf8999f93
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2023-10-29 17:27:57 +0900

    [Silver III] Title: 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/4375. 1/1.cc
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n;
+
+int main(){
+	while(cin >> n){
+		int cnt = 1, ret = 1;
+		while(true){
+			if(cnt % n == 0){
+				cout << ret << "\n";
+				break;
+			}else{
+				cnt = (cnt * 10) + 1;
+				ret++;
+				cnt %= n;
+			}
+		}
+	}
+}
