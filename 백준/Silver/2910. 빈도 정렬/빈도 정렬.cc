caecde941b8bc059a13dd91c3caef551b188c68a
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2023-11-14 20:58:14 +0900

    [Silver III] Title: 빈도 정렬, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.cc
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.cc
@@ -1,29 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-ll n, c, num;
-map<ll, int> mp, mp_first;
-vector<pair<int, ll>> v;
+int n, c, num;
+map<int, int> mp, mp_first;
+vector<pair<int, int>> v;
 
-bool cmp(pair<int, ll> a, pair<int, ll> b){
-	if(a.first == b.first){
-		return mp_first[a.second] < mp_first[b.second];
-	}else return a.first > b.first;
+bool cmp(pair<int, int> a, pair<int, int> b){
+	if(a.second == b.second){
+		return mp_first[a.first] < mp_first[b.first];
+	}else return a.second > b.second;
 }
 int main(){
 	cin >> n >> c;
 	for(int i = 0; i < n; i++){
 		cin >> num;
 		mp[num]++;
-		if(mp_first[num] == 0) mp_first[num] = i + 1;
+		if(mp_first[num] == 0) mp_first[num] = i+1;
 	}
 	for(auto it : mp){
-		v.push_back({it.second, it.first});
+		v.push_back({it.first, it.second});
 	}
 	sort(v.begin(), v.end(), cmp);
+	
 	for(auto i : v){
-		for(int j = 0; j < i.first; j++){
-			cout << i.second << " ";
+		for(int j = 0; j < i.second; j++){
+			cout << i.first << " ";
 		}
 	}
 }
