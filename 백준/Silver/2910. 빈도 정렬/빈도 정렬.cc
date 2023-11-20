8ef961c91025e35def04b39796dd8c23efef34b3
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2023-11-20 14:06:54 +0900

    [Silver III] Title: 빈도 정렬, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.cc
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.cc
@@ -1,28 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-ll n, c, num;
+int n, c, num;
 vector<int> v;
-map<int, int> mp;
-map<int, int> mp_first;
+map<int, int> mp, mp_first;
 
-bool cmp(int a, int b) {
-	if (mp[a] == mp[b]) {
+bool cmp(int a, int b){
+	if(mp[a] == mp[b]){
 		return mp_first[a] < mp_first[b];
+	}else{
+		return mp[a] > mp[b];
 	}
-	else return mp[a] > mp[b];
 }
-int main() {
+int main(){
 	cin >> n >> c;
-	for (int i = 0; i < n; i++) {
+	for(int i = 0; i < n; i++){
 		cin >> num;
-		mp[num]++;
 		v.push_back(num);
-		if (mp_first[num] == 0) mp_first[num] = i + 1;
+		mp[num]++;
+		if(mp_first[num] == 0) mp_first[num] = i+1;
 	}
 	sort(v.begin(), v.end(), cmp);
-
-	for (int i : v) {
+	
+	for(int i : v){
 		cout << i << " ";
 	}
 }
