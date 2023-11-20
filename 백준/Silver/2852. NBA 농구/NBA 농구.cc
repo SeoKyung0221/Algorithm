41ab071620011904b6f6f20f3b90c3b14991df70
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2023-11-21 00:04:05 +0900

    [Silver III] Title: NBA 농구, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2852. NBA 농구/NBA 농구.cc
+++ 백준/Silver/2852. NBA 농구/NBA 농구.cc
@@ -1,32 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n, t;
+int n, shoot, team1, team2, _prev, a, b;
 string s;
 
 int main(){
 	cin >> n;
-	int cnt1 = 0, cnt2 = 0, ret1 = 0, ret2 = 0, prev = 0;
-
 	for(int i = 0; i < n; i++){
-		cin >> t >> s;
-		int hour = atoi((s.substr(0,2)).c_str());
-		int min = atoi((s.substr(3, 2)).c_str());
-		int time = 60 * hour + min;
-		if(cnt1 > cnt2){
-			ret1 += time - prev;
-		}else if(cnt1 < cnt2){
-			ret2 += time - prev;
+		cin >> shoot >> s;
+		int hour = stoi(s.substr(0, 2));
+		int min = stoi(s.substr(3, 2));
+		int _time = hour * 60 + min;
+		if(team1 > team2){
+			a += _time - _prev;
+		}else if(team2 > team1){
+			b += _time - _prev;
 		}
-		prev = time;
-		if(t == 1) cnt1++;
-		else cnt2++;
+		_prev = _time;
+		if(shoot == 1) team1++;
+		else team2++;
 	}
-	if(cnt1 > cnt2){
-		ret1 += 48 * 60 - prev;
-	}else if(cnt1 < cnt2){
-		ret2 += 48 * 60 - prev;
+	if(team1 > team2){
+		a += 48 * 60 - _prev;
+	}else if(team2 > team1){
+		b += 48 * 60 - _prev;
 	}
-	printf("%02d:%02d\n", ret1 / 60, ret1 % 60);
-	printf("%02d:%02d\n", ret2 / 60, ret2 % 60);
-	return 0;
+	printf("%02d:%02d\n", a / 60, a % 60);
+	printf("%02d:%02d\n", b / 60, b % 60);
 }
+	
