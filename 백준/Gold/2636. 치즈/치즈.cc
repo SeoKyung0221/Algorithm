e77c77c87ebf661643ce07f758d52d5de9479b4a
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2023-11-22 17:06:16 +0900

    [Gold IV] Title: 치즈, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.cc
+++ 백준/Gold/2636. 치즈/치즈.cc
@@ -7,7 +7,6 @@ vector<pair<int, int>> v;
 
 void go(int y, int x){
 	visited[y][x] = 1;
-	
 	if(a[y][x] == 1){
 		v.push_back({y, x});
 		return;
@@ -27,35 +26,32 @@ int main(){
 			cin >> a[i][j];
 		}
 	}
-	int cnt1 = 0;
-	int cnt2 = 0;
+	int cnt = 0;
+	int cnt3 = 0;
 	while(true){
-		v.clear();
-		cnt1++;
 		memset(visited, 0, sizeof(visited));
-		int cnt3 = 0;
+		v.clear();
+		int cnt2 = 0;
+		int flag = 1;
+		cnt++;
 		for(int i = 0; i < n; i++){
 			for(int j = 0; j < m; j++){
-				if(a[i][j] == 1) cnt3++;
+				if(a[i][j] == 1) cnt2++;
 			}
 		}
 		go(0, 0);
-		for(pair<int, int> i : v){
+		for(auto i : v){
 			a[i.first][i.second] = 0;
 		}
-		int flag = 1;
 		for(int i = 0; i < n; i++){
 			for(int j = 0; j < m; j++){
-				if(a[i][j] == 1){
-					flag = 0;
-					break;
-				}
+				if(a[i][j] == 1) flag = 0;
 			}
 		}
 		if(flag){
-			cnt2 = cnt3;
+			cnt3 = cnt2;
 			break;
 		}
 	}
-	cout << cnt1 << "\n" << cnt2 << "\n";
+	cout << cnt << "\n" << cnt3 << "\n";
 }
