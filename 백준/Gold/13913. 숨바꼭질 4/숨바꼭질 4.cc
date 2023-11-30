76e48090e37d9b110ae17cac61c8f42acc432b67
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2023-12-01 00:17:05 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 16 ms, Memory: 3588 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.cc
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.cc
@@ -1,12 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define prev aaaa
-int n, k, visited[200004], prev[200004], ret;
+int n, k, visited[100004], prev[100004];
 vector<int> v;
 
 int main(){
 	cin >> n >> k;
-	
+
 	visited[n] = 1;
 	queue<int> q;
 	q.push(n);
@@ -15,18 +15,18 @@ int main(){
 		int here = q.front();
 		q.pop();
 		for(int next : {here - 1, here + 1, here * 2}){
-			if(next < 0 || next >= 200004) continue;
+			if(next < 0 || next >= 100004) continue;
 			if(visited[next]) continue;
 			visited[next] = visited[here] + 1;
 			prev[next] = here;
 			q.push(next);
 		}
 	}
+	cout << visited[k] - 1 << "\n";
 	for(int i = k; i != n; i = prev[i]){
 		v.push_back(i);
 	}
 	v.push_back(n);
-	cout << visited[k] - 1 << "\n";
 	reverse(v.begin(), v.end());
 	for(int i : v){
 		cout << i << " ";
