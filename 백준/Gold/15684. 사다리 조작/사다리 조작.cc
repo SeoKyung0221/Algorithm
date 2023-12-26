a31a47a782687400149a6f80d2101c0d480e43df
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2023-12-27 00:20:20 +0900

    [Gold III] Title: 사다리 조작, Time: 1488 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/15684. 사다리 조작/사다리 조작.cc
+++ 백준/Gold/15684. 사다리 조작/사다리 조작.cc
@@ -6,14 +6,17 @@ bool check(){
 	for(int i = 1; i <= n; i++){
 		int start = i;
 		for(int j = 1; j <= h; j++){
-			if(visited[j][start]) start++;
-			else if(visited[j][start-1]) start--;
+			if(visited[j][start-1]){
+				start--;
+			}else if(visited[j][start]){
+				start++;
+			}
 		}
 		if(start != i) return false;
 	}
 	return true;
 }
-void go(int here, int cnt){
+void go(int idx, int cnt){
 	if(cnt > 3 || cnt >= ret) return;
 	
 	if(check()){
@@ -21,15 +24,14 @@ void go(int here, int cnt){
 		return;
 	}
 	
-	for(int i = here; i <= h; i++){
+	for(int i = idx; i <= h; i++){
 		for(int j = 1; j <= n; j++){
 			if(visited[i][j-1] || visited[i][j] || visited[i][j+1]) continue;
 			visited[i][j] = 1;
-			go(i, cnt+1);
+			go(i, cnt + 1);
 			visited[i][j] = 0;
 		}
 	}
-	return;
 }
 int main(){
 	cin >> n >> m >> h;
@@ -38,5 +40,6 @@ int main(){
 		visited[a][b] = 1;
 	}
 	go(1, 0);
+	
 	cout << ((ret == 1e9) ? -1 : ret) << "\n";
 }
