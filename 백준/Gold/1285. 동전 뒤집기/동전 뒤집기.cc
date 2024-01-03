abd1f1c0b0f8b60256bd127703a4cce78bd9cdb5
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2024-01-04 00:04:24 +0900

    [Gold I] Title: 동전 뒤집기, Time: 340 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/1285. 동전 뒤집기/동전 뒤집기.cc
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n, a[24], ret = 1e9;
+string s;
+
+void go(int idx){
+	if(idx == n){
+		int sum = 0;
+		for(int i = 0; i < n; i++){
+			int cnt = 0;
+			for(int j = 0; j < n; j++){
+				if(a[j] & (1 << i)) cnt++;
+			}
+			sum += min(cnt, n - cnt);
+		}
+		ret = min(ret, sum);
+		return;
+	}
+	go(idx + 1);
+	a[idx] = ~a[idx];
+	go(idx + 1); 
+}
+int main(){
+	cin >> n;
+	for(int i = 0; i < n; i++){
+		cin >> s;
+		int value = 1;
+		for(int j = 0; j < s.size(); j++){
+			if(s[j] == 'T') a[i] |= value;
+			value *= 2;
+		}
+	}
+	go(0);
+	
+	cout << ret << "\n";
+	return 0;
+}
