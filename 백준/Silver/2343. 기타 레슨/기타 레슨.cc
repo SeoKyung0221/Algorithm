e3a8ec87ae5371f0b8547dae33b2f9ae0546f0c9
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2024-01-04 22:13:53 +0900

    [Silver I] Title: 기타 레슨, Time: 32 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/2343. 기타 레슨/기타 레슨.cc
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.cc
@@ -1,20 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-ll n, m, mx, a[100004], ret = 1e9;
+int n, m, a[100004], mx, ret;
 
 bool check(ll mid){
-	if(mid < mx) return false;
+	ll ans = 1;
 	ll temp = mid;
-	ll cnt = 1;
 	for(int i = 0; i < n; i++){
-		if(temp - a[i] < 0){
-			cnt++;
+		if(temp >= a[i]){
+			temp -= a[i];
+		}else{
+			ans++;
 			temp = mid;
 			temp -= a[i];
-		}else temp -= a[i];
+		}
 	}
-	return cnt <= m;
+	return ans <= m;
 }
 int main(){
 	cin >> n >> m;
@@ -22,13 +23,15 @@ int main(){
 		cin >> a[i];
 		mx = max(mx, a[i]);
 	}
-	ll l = 0, h = 1e9;
+	ll l = mx, h = 1e9;
 	while(l <= h){
 		ll mid = (l + h) / 2;
 		if(check(mid)){
-			ret = min(ret, mid);
-			h = mid - 1;			
-		}else l = mid + 1;
+			ret = mid;
+			h = mid -1;
+		}else{
+			l = mid + 1;
+		}
 	}
 	cout << ret << "\n";
 }
