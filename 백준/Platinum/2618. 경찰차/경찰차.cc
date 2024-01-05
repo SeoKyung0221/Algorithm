29467f9d6823f70bab78eff476ba9f47a4197f7c
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2024-01-05 21:46:40 +0900

    [Platinum IV] Title: 경찰차, Time: 20 ms, Memory: 5964 KB -BaekjoonHub

--- 백준/Platinum/2618. 경찰차/경찰차.cc
+++ 백준/Platinum/2618. 경찰차/경찰차.cc
@@ -3,41 +3,43 @@ using namespace std;
 int n, w, p, q, dp[1004][1004];
 pair<int, int> a[1004];
 
+int dist(int p, int q){
+	return abs(a[p].first - a[q].first) + abs(a[p].second - a[q].second);
+}
 int go(int one, int two){
 	if(one == w + 2 || two == w + 2){
 		return 0;
 	}
 	int &ret = dp[one][two];
-	if(ret != 0) return ret;
+	if(ret != -1) return ret;
 	
+	ret = 0;
 	int next = max(one, two) + 1;
-
-	int p = go(next, two) + (abs(a[next].first - a[one].first) + abs(a[next].second - a[one].second));
-	int q = go(one, next) + (abs(a[next].first - a[two].first) + abs(a[next].second - a[two].second));
-
-	ret = min(p, q);
+	
+	ret = min(go(next, two) + dist(one, next), go(one, next) + dist(two, next));
+	
 	return ret;
 }
 int main(){
 	cin >> n >> w;
-	a[1] = make_pair(1, 1);
-	a[2] = make_pair(n, n);
+	memset(dp, -1, sizeof(dp));
+	a[1].first = 1, a[1].second = 1;
+	a[2].first = n, a[2].second = n;
 	for(int i = 3; i <= w + 2; i++){
 		cin >> p >> q;
-		a[i] = make_pair(p, q);
+		a[i].first = p, a[i].second = q;
 	}
 	cout << go(1, 2) << "\n";
 	
 	int one = 1;
 	int two = 2;
 	for(int i = 3; i <= w + 2; i++){
-		if(dp[one][i] + (abs(a[i].first - a[two].first) + abs(a[i].second - a[two].second)) > dp[i][two] + (abs(a[i].first - a[one].first) + abs(a[i].second - a[one].second))){
-			cout << "1\n";
-			one = i; 
-		}else{
-			cout << "2\n";
+		if(dp[i][two] + dist(one, i) > dp[one][i] + dist(two, i)){
 			two = i;
+			cout << 2 << "\n";
+		}else{
+			one = i;
+			cout << 1 << "\n";
 		}
 	}
-	return 0;
 }
