ce761eafe513b84375f3afbd172c636ff9c11f86
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2024-01-08 04:12:13 +0900

    [Gold II] Title: 게임, Time: 0 ms, Memory: 2048 KB -BaekjoonHub

--- 백준/Gold/1103. 게임/게임.cc
+++ 백준/Gold/1103. 게임/게임.cc
@@ -1,37 +1,40 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n, m, visited[54][54], dp[54][54];
-string s;
-char a[54][54];
 const int dy[] = {-1, 0, 1, 0};
 const int dx[] = {0, 1, 0, -1};
+int n, m, ret, visited[54][54], flag, dp[54][54];
+char a[54][54];
 
-int solve(int y, int x, char value){
-	if(y < 0 || y >= n || x < 0 || x >= m || value == 'H') return 0;
-	if(visited[y][x] == 1){
-		cout << "-1\n";
+int go(int y, int x){
+	if(y < 0 || y >= n || x < 0 || x >= m || a[y][x] == 'H'){
+		return 0;
+	}
+	if(visited[y][x]){
+		cout << -1 << "\n";
 		exit(0);
 	}
 	int &ret = dp[y][x];
-	if(ret != 0) return ret;
-
-	int v = value - '0';
+	if(ret != -1) return ret;
+	
+	ret = 0;
+	
 	visited[y][x] = 1;
+	int cnt = a[y][x] - '0';
 	for(int i = 0; i < 4; i++){
-		int ny = y + v * dy[i];
-		int nx = x + v * dx[i];
-		ret = max(ret, solve(ny, nx, a[ny][nx]) + 1);
+		int ny = y + dy[i] * cnt;
+		int nx = x + dx[i] * cnt;
+		ret = max(ret, go(ny, nx) + 1);
 	}
 	visited[y][x] = 0;
 	return ret;
 }
 int main(){
+	memset(dp, -1, sizeof(dp));
 	cin >> n >> m;
 	for(int i = 0; i < n; i++){
-		cin >> s;
 		for(int j = 0; j < m; j++){
-			a[i][j] = s[j];
+			cin >> a[i][j];
 		}
 	}
-	cout << solve(0, 0, a[0][0]) << "\n";
+	cout << go(0, 0) << "\n";
 }
