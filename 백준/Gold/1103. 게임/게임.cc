fca4b4a8d4d39475af7611c16562c26fba34dadd
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2024-01-09 00:47:58 +0900

    [Gold II] Title: 게임, Time: 0 ms, Memory: 2048 KB -BaekjoonHub

--- 백준/Gold/1103. 게임/게임.cc
+++ 백준/Gold/1103. 게임/게임.cc
@@ -2,7 +2,7 @@
 using namespace std;
 const int dy[] = {-1, 0, 1, 0};
 const int dx[] = {0, 1, 0, -1};
-int n, m, ret, visited[54][54], flag, dp[54][54];
+int n, m, visited[54][54], dp[54][54], ret;
 char a[54][54];
 
 int go(int y, int x){
@@ -17,9 +17,8 @@ int go(int y, int x){
 	if(ret != -1) return ret;
 	
 	ret = 0;
-	
-	visited[y][x] = 1;
 	int cnt = a[y][x] - '0';
+	visited[y][x] = 1;
 	for(int i = 0; i < 4; i++){
 		int ny = y + dy[i] * cnt;
 		int nx = x + dx[i] * cnt;
