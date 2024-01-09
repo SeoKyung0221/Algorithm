a886094c7613d4407e69754b1d220c3f338a1230
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2024-01-09 18:45:56 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
@@ -5,50 +5,64 @@ const int dx[] = {1, 1, 0};
 int n, a[18][18], dp[18][18][4];
 
 int go(int y, int x, int flag){
-	if(y < 0 || y >= n || x < 0 || x >= n){
-		return 0;
-	}
+	if(y < 0 || y >= n || x < 0 || x >= n) return 0;
 	if(y == n-1 && x == n-1){
 		return 1;
 	}
 	int &ret = dp[y][x][flag];
-	if(ret != 0) return ret;
+	if(ret != -1) return ret;
 	
+	ret = 0;
 	if(flag == 1){
-		if(a[y][x+1] != 1)	ret += go(y, x + 1, 1);
-
-		int cnt = 1;
-		for(int i = 0; i < 3; i++){
-			int ny = y + dy[i];
-			int nx = x + dx[i];
-			if(a[ny][nx] == 1) cnt = 0;
+		if(a[y][x + 1] != 1){
+			ret += go(y, x + 1, 1);
 		}
-		if(cnt)	ret += go(y + 1, x + 1, 3);
-	}else if(flag == 2){
-		if(a[y+1][x] != 1) ret += go(y + 1, x, 2); 
-		
 		int cnt = 1;
 		for(int i = 0; i < 3; i++){
 			int ny = y + dy[i];
 			int nx = x + dx[i];
-			if(a[ny][nx] == 1) cnt = 0;
+			if(a[ny][nx] == 1){
+				cnt = 0;
+				break;
+			}
+		}
+		if(cnt) ret += go(y + 1, x + 1, 3);
+	}else if(flag == 2){
+		if(a[y + 1][x] != 1){
+			ret += go(y + 1, x, 2);
+		}
+		int cnt = 1;
+		for(int i = 0; i < 3; i++){
+			int ny = y + dy[i];
+			int nx = x + dx[i];
+			if(a[ny][nx] == 1){
+				cnt = 0;
+				break;
+			}
 		}
 		if(cnt) ret += go(y + 1, x + 1, 3);
 	}else if(flag == 3){
-		if(a[y][x+1] != 1) ret += go(y, x + 1, 1);
-		if(a[y+1][x] != 1) ret += go(y + 1, x, 2);
-		
+		if(a[y][x + 1] != 1){
+			ret += go(y, x + 1, 1);
+		}
+		if(a[y + 1][x] != 1){
+			ret += go(y + 1, x, 2);
+		}
 		int cnt = 1;
 		for(int i = 0; i < 3; i++){
 			int ny = y + dy[i];
 			int nx = x + dx[i];
-			if(a[ny][nx] == 1) cnt = 0;
+			if(a[ny][nx] == 1){
+				cnt = 0;
+				break;
+			}
 		}
 		if(cnt) ret += go(y + 1, x + 1, 3);
 	}
 	return ret;
 }
 int main(){
+	memset(dp, -1, sizeof(dp));
 	cin >> n;
 	for(int i = 0; i < n; i++){
 		for(int j = 0; j < n; j++){
@@ -56,5 +70,4 @@ int main(){
 		}
 	}
 	cout << go(0, 1, 1) << "\n";
-	return 0;
 }
