50b10c9b7c7463ebb54b8e2d48be8b4708c91612
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2024-01-11 23:07:31 +0900

    [Gold IV] Title: 플로이드, Time: 84 ms, Memory: 2064 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.cc
+++ 백준/Gold/11404. 플로이드/플로이드.cc
@@ -1,18 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int INF = 987654321;
-int n, m, a, b, c, dist[104][104];
+int n, m, dist[104][104], a, b, c;
 
 int main(){
-	fill(&dist[0][0], &dist[0][0] + 104 * 104, INF);
 	cin >> n >> m;
+	
+	fill(&dist[0][0], &dist[0][0] + 104 * 104, 1e9);
+	
 	for(int i = 0; i < m; i++){
 		cin >> a >> b >> c;
 		dist[a][b] = min(dist[a][b], c);
 	}
+	
 	for(int i = 1; i <= n; i++){
 		dist[i][i] = 0;
 	}
+	
 	for(int k = 1; k <= n; k++){
 		for(int i = 1; i <= n; i++){
 			for(int j = 1; j <= n; j++){
@@ -22,9 +25,12 @@ int main(){
 			}
 		}
 	}
+
 	for(int i = 1; i <= n; i++){
 		for(int j = 1; j <= n; j++){
-			cout << (dist[i][j] == INF ? 0 : dist[i][j]) << " ";
+			if(dist[i][j] == 1e9){
+				cout << 0 << " ";
+			}else cout << dist[i][j] << " ";
 		}
 		cout << "\n";
 	}
