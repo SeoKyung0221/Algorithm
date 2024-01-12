60c0943b806dba53ffc6dc491e2fb99cc2ff8479
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2024-01-12 17:14:07 +0900

    [Gold III] Title: 경사로, Time: 0 ms, Memory: 2104 KB -BaekjoonHub

--- 백준/Gold/14890. 경사로/경사로.cc
+++ 백준/Gold/14890. 경사로/경사로.cc
@@ -1,16 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n, l, a[104][104], b[104][104], ret;
+int n, l, ret, a[104][104], b[104][104];
 
-void go(int a[104][104]){
+void solve(int a[104][104]){
 	for(int i = 0; i < n; i++){
 		int cnt = 1;
 		int j;
 		for(j = 0; j < n-1; j++){
 			if(a[i][j] == a[i][j+1]) cnt++;
-			else if(a[i][j] + 1 == a[i][j+1] && cnt >= l) cnt = 1;
-			else if(a[i][j] - 1 == a[i][j+1] && cnt >= 0) cnt = 1 - l;
-			else break;  
+			else if(a[i][j] + 1 == a[i][j+1] && cnt >= l){
+				cnt = 1;
+			}else if(a[i][j] - 1 == a[i][j+1] && cnt >= 0){
+				cnt = 1 - l;
+			}else break;
 		}
 		if(j == n-1 && cnt >= 0){
 			ret++;
@@ -25,7 +27,7 @@ int main(){
 			b[j][i] = a[i][j];
 		}
 	}
-	go(a);
-	go(b);
+	solve(a);
+	solve(b);
 	cout << ret << "\n";
 }
