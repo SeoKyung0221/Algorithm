a2a0f9bc1880655ab59274945ece688a339f6ecc
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2024-01-13 20:00:02 +0900

    [Gold V] Title: 자두나무, Time: 0 ms, Memory: 2276 KB -BaekjoonHub

--- 백준/Gold/2240. 자두나무/자두나무.cc
+++ 백준/Gold/2240. 자두나무/자두나무.cc
@@ -1,19 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
-int dp[1004][2][34], n, m, b[1004];
+int t, w, a[1004], dp[1004][32][2];
 
-int go(int idx, int tree, int cnt){
-	if(cnt < 0) return -1e9;
-	if(idx == n) return 0;
-    int &ret = dp[idx][tree][cnt];
-    if(~ret) return ret;  
-    return ret = max(go(idx + 1, tree^1, cnt - 1), go(idx + 1, tree, cnt)) + (tree == b[idx] - 1); 
+int go(int idx, int move, int flag){
+	if(idx == t){
+		return 0;
+	}
+	if(move > w) return -1e9;
+	
+	int &ret = dp[idx][move][flag];
+	if(ret != -1) return ret;
+	
+	int _flag = (flag == 1) ? 2 : 1;
+	
+	if(a[idx] == _flag){
+		ret = max(go(idx + 1, move + 1, !flag), go(idx + 1, move, flag)) + 1;
+	}else{
+		ret = max(go(idx + 1, move + 1, !flag), go(idx + 1, move, flag));
+	}
+	return ret;
 }
-
 int main(){
-	memset(dp,-1,sizeof(dp));
-	cin >> n >> m;
-    for(int i = 0; i < n; i++) cin >> b[i]; 
-    cout << max(go(0, 1, m - 1), go(0, 0, m)) << '\n';
-    return 0;
+	memset(dp, -1, sizeof(dp));
+	cin >> t >> w;
+	for(int i = 0; i < t; i++){
+		cin >> a[i];
+	}
+	cout << max(go(0, 0, 0), go(0, 1, 1)) << "\n";
 }
