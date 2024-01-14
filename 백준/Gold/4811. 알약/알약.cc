a2e8b04b14510839c3e64bf63081da545e69aac2
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2024-01-14 21:44:09 +0900

    [Gold V] Title: 알약, Time: 28 ms, Memory: 2040 KB -BaekjoonHub

--- 백준/Gold/4811. 알약/알약.cc
+++ 백준/Gold/4811. 알약/알약.cc
@@ -1,25 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-ll n, dist[64][64];
+ll n, dp[34][68]; // 전체알, 반알 
 
-ll go(int w, int h){
-	if(w == 0 && h == 0) return 1;
-	if(w < 0 || h < 0) return 0;
+ll go(int one, int half){
+	if(one == 0 && half == 0){
+		return 1;
+	}
+	if(one == 0) return go(one, half - 1);
+	if(half == 0) return go(one - 1, half + 1);
 	
-	ll &ret = dist[w][h];
-	if(ret != 0) return ret;
+	ll &ret = dp[one][half];
+	if(ret != -1) return ret;
 	
 	ret = 0;
-	ret = go(w - 1, h + 1) + go(w, h - 1);
 	
-	return ret;
+	return ret = go(one - 1, half + 1) + go(one, half - 1);
 }
 int main(){
 	while(true){
-		fill(&dist[0][0], &dist[0][0] + 64 * 64, 0);
+		memset(dp, -1, sizeof(dp));
 		cin >> n;
-		if(n == 0) break;
-		cout << go(n, 0) << "\n";
+		if(n == 0) return 0;
+		
+		cout << go(n, 0) << "\n"; 
 	}
 }
