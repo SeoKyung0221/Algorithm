6a20c422fb75009660882ea884b51a58a13c7e33
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2024-01-16 00:13:38 +0900

    [Platinum V] Title: 인기가 넘쳐흘러, Time: 0 ms, Memory: 2384 KB -BaekjoonHub

--- 백준/Platinum/17258. 인기가 넘쳐흘러/인기가 넘쳐흘러.cc
+++ 백준/Platinum/17258. 인기가 넘쳐흘러/인기가 넘쳐흘러.cc
@@ -3,22 +3,21 @@ using namespace std;
 int n, m, k, t, a, b, cnt[304], dp[304][304];
 vector<pair<int, int>> v;
 
-int solve(int idx, int num, int prev){
-	if(idx == v.size()) return 0;
+int go(int idx, int num, int prev){
+	if(idx == v.size()){
+		return 0;
+	}
 	int &ret = dp[idx][num];
 	if(ret != -1) return ret;
 	
 	ret = 0;
 	
-	int cost = max(0, t - v[idx].second);
+	int cost = max(0, t - v[idx].first);
 	int real_cost = (prev >= cost) ? 0 : cost - prev;
 	
 	if(num - real_cost >= 0){
-		ret = max(solve(idx + 1, num - real_cost, cost) + v[idx].first, solve(idx + 1, num, 0));
-	}else{
-		ret = max(ret, solve(idx + 1, num, 0));
-	}
-	return ret;
+		return ret = max(go(idx + 1, num - real_cost, cost) + v[idx].second, go(idx + 1, num, 0));
+	}else return ret = go(idx + 1, num, 0);
 }
 int main(){
 	memset(dp, -1, sizeof(dp));
@@ -29,17 +28,17 @@ int main(){
 			cnt[j] = min(t, ++cnt[j]);
 		}
 	}
-	int _cnt = 1;
 	int temp = cnt[1];
+	int _cnt = 1;
 	for(int i = 2; i <= n; i++){
 		if(temp != cnt[i]){
-			v.push_back({_cnt, temp});
+			v.push_back({temp, _cnt});
 			temp = cnt[i];
 			_cnt = 0;
 		}
 		_cnt++;
 	}
-	v.push_back({_cnt, temp});
-	
-	cout << solve(0, k, 0) << "\n";
+	v.push_back({temp, _cnt});
+	cout << go(0, k, 0) << "\n";
+	return 0;
 }
