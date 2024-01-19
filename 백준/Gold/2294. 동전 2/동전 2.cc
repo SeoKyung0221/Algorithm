9fb563b1c7d287ea00e5c48187daf83fc0936d04
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2024-01-20 03:58:37 +0900

    [Gold V] Title: 동전 2, Time: 4 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.cc
+++ 백준/Gold/2294. 동전 2/동전 2.cc
@@ -1,17 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int INF = 1e9;
-int n, k, num, dp[10004];
+int n, k, temp, dp[10004];
 
 int main(){
-	fill(dp, dp+10004, INF);
-	cin >> n >> k;
+	fill(dp, dp + 10004, 1e9);
 	dp[0] = 0;
+	cin >> n >> k;
 	for(int i = 0; i < n; i++){
-		cin >> num;
-		for(int j = num; j <= k; j++){
-			dp[j] = min(dp[j], dp[j-num] + 1);
+		cin >> temp;
+		for(int j = temp; j <= k; j++){
+			dp[j] = min(dp[j], dp[j - temp] + 1);
 		}
 	}
-	cout << ((dp[k] == INF) ? -1 : dp[k]) << "\n";
+	cout << ((dp[k] == 1e9) ? -1 : dp[k]) << "\n";
 }
