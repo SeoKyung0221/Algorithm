762c095ebac8fafc0d6f62e750a13079d2a90cd9
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2024-01-22 23:08:01 +0900

    [Gold III] Title: 성곽, Time: 0 ms, Memory: 2052 KB -BaekjoonHub

--- 백준/Gold/2234. 성곽/성곽.cc
+++ 백준/Gold/2234. 성곽/성곽.cc
@@ -2,57 +2,56 @@
 using namespace std;
 const int dy[] = {0, -1, 0, 1};
 const int dx[] = {-1, 0, 1, 0};
-int n, m, a[54][54], cnt, ret2, ret3, combSize[2504], visited[54][54];
+int n, m, a[54][54], visited[54][54], comp[2504], ret1, ret2, ret3;
 
-int dfs(int y, int x){
+int dfs(int y, int x, int cnt){
 	int ret = 1;
-	
 	visited[y][x] = cnt;
+	
 	for(int i = 0; i < 4; i++){
 		if(!(a[y][x] & (1 << i))){
 			int ny = y + dy[i];
 			int nx = x + dx[i];
-			if(ny < 0 || ny >= m || nx < 0 || nx >= n) continue;
+			if(ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
 			if(visited[ny][nx]) continue;
-			ret += dfs(ny, nx);
+			ret += dfs(ny, nx, cnt);
 		}
 	}
 	return ret;
 }
 int main(){
-	cin >> n >> m;
-	for(int i = 0; i < m; i++){
-		for(int j = 0; j < n; j++){
+	cin >> m >> n;
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < m; j++){
 			cin >> a[i][j];
 		}
 	}
-	for(int i = 0; i < m; i++){
-		for(int j = 0; j < n; j++){
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < m; j++){
 			if(!visited[i][j]){
-				cnt++;
-				combSize[cnt] = dfs(i, j);
-				ret2 = max(ret2, combSize[cnt]);
+				ret1++;
+				comp[ret1] = dfs(i, j, ret1);
+				ret2 = max(ret2, comp[ret1]);
 			}
 		}
 	}
-	for(int i = 0; i < m; i++){
-		for(int j = 0; j < n; j++){
-			if(i + 1 < m){
-				int b = visited[i][j];
-				int c = visited[i+1][j];
-				if(b != c){
-					ret3 = max(ret3, combSize[b] + combSize[c]);
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < m; j++){
+			if(i + 1 < n){
+				int a = visited[i][j];
+				int b = visited[i + 1][j];
+				if(a != b){
+					ret3 = max(ret3, comp[a] + comp[b]);
 				}
 			}
-			if(j + 1 < n){
-				int b = visited[i][j];
-				int c = visited[i][j+1];
-				if(b != c){
-					ret3 = max(ret3, combSize[b] + combSize[c]);
+			if(j + 1 < m){
+				int a = visited[i][j];
+				int b = visited[i][j + 1];
+				if(a != b){
+					ret3 = max(ret3, comp[a] + comp[b]);
 				}
 			}
 		}
 	}
-	cout << cnt << "\n" << ret2 << "\n" << ret3 << "\n";
-	return 0;
+	cout << ret1 << "\n" << ret2 << "\n" << ret3 << "\n";
 }
