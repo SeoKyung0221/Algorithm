80898a6c59c58c7a356fd5efce5df5fb5e4fbd9b
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2024-01-24 02:32:17 +0900

    [Gold V] Title: 동전 1, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.cc
+++ 백준/Gold/2293. 동전 1/동전 1.cc
@@ -3,12 +3,10 @@ using namespace std;
 int n, k, temp, dp[10004];
 
 int main(){
-	memset(dp, 0, sizeof(dp));
 	cin >> n >> k;
-	
+	dp[0] = 1;
 	for(int i = 0; i < n; i++){
 		cin >> temp;
-		dp[0] = 1;
 		for(int j = temp; j <= k; j++){
 			dp[j] += dp[j - temp];
 		}
