62a0241f66b817032104acc8a2cadb961abe361d
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2024-01-24 01:03:13 +0900

    [Gold IV] Title: 가르침, Time: 128 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1062. 가르침/가르침.cc
+++ 백준/Gold/1062. 가르침/가르침.cc
@@ -1,31 +1,32 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-int n, m, words[51];
-string s; 
-int count(int mask) {
-    int cnt = 0;
-    for (int word : words) { 
-        if(word && (word & mask) == word)cnt++; 
-    }
-    return cnt;
+int n, k, a[54], ret;
+string s;
+
+int count(int mask){
+	int ret = 0;
+	for(int i = 0; i < n; i++){
+		if(mask && (mask & a[i]) == a[i]) ret++; 
+	}
+	return ret;
 }
-int go(int index, int k, int mask) {
-    if (k < 0) return 0;
-    if (index == 26) return count(mask); 
-    int ret = go(index+1, k-1, mask | (1 << index)); 
-    if (index != 'a'-'a' && index != 'n'-'a' && index != 't'-'a' && index != 'i'-'a' && index != 'c'-'a') {
-        ret = max(ret, go(index+1, k, mask)); 
-    }
-    return ret;
+int go(int idx, int cnt, int mask){
+	if(cnt < 0) return 0;
+	if(idx == 26) return count(mask);
+	int ret = go(idx + 1, cnt - 1, mask | (1 << idx));
+	if('a' + idx != 'a' && 'a' + idx != 'n' && 'a' + idx != 't' && 'a' + idx != 'i' && 'a' + idx != 'c'){
+		ret = max(ret, go(idx + 1, cnt, mask));
+	}
+	return ret;
 }
-int main() { 
-    cin >> n >> m; 
-    for (int i=0; i<n; i++) { 
-        cin >> s;
-        for (char str : s) {
-            words[i] |= (1 << (str - 'a'));
-        }
-    }
-    cout << go(0, m, 0) << '\n';
-    return 0;
+int main(){
+	cin >> n >> k;
+	for(int i = 0; i < n; i++){
+		cin >> s;
+		for(char str : s){
+			a[i] |= (1 << (str - 'a'));
+		}
+	}
+	
+	cout << go(0, k, 0) << "\n";
 }
