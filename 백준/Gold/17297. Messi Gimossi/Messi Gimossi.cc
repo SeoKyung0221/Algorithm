a079d16d9a684b53f2f792b836734fffe1568107
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2024-01-24 23:40:39 +0900

    [Gold IV] Title: Messi Gimossi, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/17297. Messi Gimossi/Messi Gimossi.cc
+++ 백준/Gold/17297. Messi Gimossi/Messi Gimossi.cc
@@ -1,18 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
-int m, dp[40];
+int n, dp[40];
 string s = "Messi Gimossi";
-
 int main(){
-	cin >> m;
+	cin >> n;
+	
 	dp[1] = 5;
 	dp[2] = 13;
-	for(int i = 3; i <= 39; i++){
+	for(int i = 3; i < 40; i++){
 		dp[i] = dp[i-1] + dp[i-2] + 1;
 	}
 	for(int i = 39; i >= 2; i--){
-		if(m > dp[i]) m -= dp[i] + 1;
+		if(n > dp[i]) n -= (dp[i] + 1);
 	}
-	if(m == 0 || m == 6) cout << "Messi Messi Gimossi" << "\n";
-	else cout << s[m-1] << "\n";
+	if(n == 6 || n == 0) cout << "Messi Messi Gimossi\n";
+	else cout << s[n - 1] << "\n";
 }
