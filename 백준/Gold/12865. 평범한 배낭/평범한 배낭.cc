b8be518c52295152a6625c630a3fca6c4f803cd2
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2024-01-29 21:53:50 +0900

    [Gold V] Title: 평범한 배낭, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,14 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n, k, dp[100004], w, v;
+int n, k, w, v, dp[100004];
 
 int main(){
 	cin >> n >> k;
 	for(int i = 0; i < n; i++){
 		cin >> w >> v;
-		if(w > k) continue;
 		for(int j = k; j >= w; j--){
-			dp[j] = max(dp[j], dp[j-w] + v);
+			dp[j] = max(dp[j], dp[j - w] + v);
 		}
 	}
 	cout << dp[k] << "\n";
