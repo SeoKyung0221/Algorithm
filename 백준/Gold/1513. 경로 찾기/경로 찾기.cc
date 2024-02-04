378bf41fda375ec6f03ad23e890ba9fa3f3b9901
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2024-02-05 00:10:14 +0900

    [Gold II] Title: 경로 찾기, Time: 92 ms, Memory: 35248 KB -BaekjoonHub

--- 백준/Gold/1513. 경로 찾기/경로 찾기.cc
+++ 백준/Gold/1513. 경로 찾기/경로 찾기.cc
@@ -1,37 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
+int n, m, c, dp[54][54][54][54], a[54][54], q, w;
 const int mod = 1000007;
-int n, m, c, a[54][54], b, d, dp[54][54][54][54];
 
-int go(int y, int x, int cnt, int prev){
+int go(int y, int x, int prev, int cnt){
 	if(y > n || x > m) return 0;
 	if(y == n && x == m){
-		if(cnt == 0 && a[y][x] == 0) return 1;
-		if(cnt == 1 && a[y][x] > prev) return 1;
+		if(cnt == 0 && a[n][m] == 0) return 1;
+		if(cnt == 1 && a[n][m] > prev) return 1;
 		return 0;
 	}
 	
-	int &ret = dp[y][x][cnt][prev];
+	int &ret = dp[y][x][prev][cnt];
 	if(ret != -1) return ret;
 	
 	ret = 0;
+	
 	if(a[y][x] == 0){
-		ret = go(y + 1, x, cnt, prev) + go(y, x + 1, cnt, prev);
-		ret %= mod;
+		ret = (go(y + 1, x, prev, cnt) + go(y, x+ 1, prev, cnt)) % mod;
 	}else if(a[y][x] > prev){
-		ret = go(y + 1, x, cnt - 1, a[y][x]) + go(y, x + 1, cnt - 1, a[y][x]);
-		ret %= mod;
+		ret = (go(y + 1, x, a[y][x], cnt - 1) + go(y, x + 1, a[y][x], cnt - 1)) % mod;
 	}
 	return ret;
 }
+
 int main(){
-	fill(&dp[0][0][0][0], &dp[0][0][0][0] + 54 * 54 * 54 * 54, -1);
+	memset(dp, -1, sizeof(dp));
 	cin >> n >> m >> c;
 	for(int i = 1; i <= c; i++){
-		cin >> b >> d;
-		a[b][d] = i;
+		cin >> q >> w;
+		a[q][w] = i;
 	}
 	for(int i = 0; i <= c; i++){
-		cout << go(1, 1, i, 0) << " ";
+		cout << go(1, 1, 0, i) << " ";
 	}
 }
