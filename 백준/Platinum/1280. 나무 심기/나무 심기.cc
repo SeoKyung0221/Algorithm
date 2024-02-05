d0a611e034c7a12812ad2fe225a743bba3cbcc0d
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2024-02-06 01:16:07 +0900

    [Platinum IV] Title: 나무 심기, Time: 120 ms, Memory: 5148 KB -BaekjoonHub

--- 백준/Platinum/1280. 나무 심기/나무 심기.cc
+++ 백준/Platinum/1280. 나무 심기/나무 심기.cc
@@ -1,45 +1,48 @@
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-const int mod = 1e9 + 7;
-const int MAX_N = 200004;
-int n, value;
-ll ret = 1;
+const ll mod = 1e9 + 7;
+ll n, num, ret = 1;
 
 ll sum(vector<ll> &tree, int i){
-    ll ans = 0;
-    while(i > 0){
-        ans += tree[i];
-        i -= (i & -i);
-    }
-    return ans;
+	ll ret = 0;
+	
+	while(i > 0){
+		ret += tree[i];
+		i -= (i & -i);
+	}
+	return ret;
+}
+
+void update(vector<ll> &tree, int i, ll diff){
+	while(i < tree.size()){
+		tree[i] += diff;
+		i += (i & -i);
+	}
+	return;
 }
 
 ll _sum(vector<ll> &tree, int s, int e){
-    if(s > e) return 0;
-    return sum(tree, e) - sum(tree, s-1);
+	return sum(tree, e) - sum(tree, s - 1);
 }
-void update(vector<ll> &tree, int i, int value){
-    while(i < tree.size()){
-        tree[i] += value;
-        i += (i & -i);
-    }
-    return;
-}
+
 int main(){
-    cin >> n;
-    vector<ll> tree_cnt(MAX_N, 0);
-    vector<ll> tree(MAX_N, 0);
-    for(int i = 1; i <= n; i++){
-        cin >> value; value++;
-        if(i != 1){
-            ll l = value * _sum(tree_cnt, 1, value - 1) - _sum(tree, 1, value - 1);
-            ll r = _sum(tree, value + 1, MAX_N) - value * _sum(tree_cnt, value + 1, MAX_N);
-            ret *= (l + r) % mod;
-            ret %= mod;
-        }
-        update(tree_cnt, value, 1);
-        update(tree, value, value);
-    }
-    cout << ret << "\n";
+	cin >> n;
+	
+	vector<ll> tree_cnt(200004, 0);
+	vector<ll> tree(200004, 0);
+	for(int i = 1; i <= n; i++){
+		cin >> num; num++;
+		
+		if(i != 1){
+			ll left = (_sum(tree_cnt, 1, num - 1) * num - _sum(tree, 1, num - 1)) % mod;
+			ll right = (_sum(tree, num + 1, 200004) - _sum(tree_cnt, num + 1, 200004) * num) % mod;
+			ret *= (left + right) % mod;
+			ret %= mod;
+		}
+		
+		update(tree_cnt, num, 1);
+		update(tree, num, num);
+	}
+	cout << ret << "\n";
 }
