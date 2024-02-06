7b8dc45d37ac96ce95e8ad50e60640584aacea95
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2024-02-06 17:00:31 +0900

    [Platinum IV] Title: 구간 합 구하기 3, Time: 204 ms, Memory: 18532 KB -BaekjoonHub

--- 백준/Platinum/11658. 구간 합 구하기 3/구간 합 구하기 3.cc
+++ 백준/Platinum/11658. 구간 합 구하기 3/구간 합 구하기 3.cc
@@ -1,52 +1,52 @@
-#include<cstdio>
-#define max_n 1025
-int n, m;
-int a[max_n][max_n], bit[max_n][max_n];
-void update(int y, int x, int value){
-    while(y <= n){
-        int j = x;
-        while(j <= n){
-            bit[y][j] += value;
-            j += (j & -j);
-        }
-        y += (y & -y);
-    }
-    return;
-}
-int sum(int y, int x) {
-    int s = 0;
-    while(y > 0){
-        int j = x;
-        while(j > 0){
-            s += bit[y][j];
-            j -= (j & -j);
-        }
-        y -= (y&-y);
-    }
-    return s;
-}
-int main() {
-    scanf("%d%d", &n, &m);
-    for (int i = 1; i <= n; i++){
-        for (int j = 1; j <= n; j++){
-            scanf("%d", &a[i][j]);
-            update(i, j, a[i][j]);
-        }
-    }
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define y1 aaaa
+ll n, m, a[1028][1028], b[1028][1028], w, x, y, c, x1, y1, x2, y2;
 
-    for (int i = 0; i < m; i++) {
-        int type;
-        scanf("%d", &type);
-        if (type) {
-            int y1, x1, y2, x2;
-            scanf("%d%d%d%d",&y1, &x1, &y2, &x2);
-            printf("%d\n", sum(y2, x2) - sum(y1 - 1, x2) - sum(y2, x1 - 1) + sum(y1 - 1, x1 - 1));
-        }
-        else{
-            int y, x, value;
-            scanf("%d%d%d",&y, &x, &value);
-            update(y, x, value - a[y][x]), a[y][x] = value;
-        }
-    }
-    return 0;
+ll sum(ll b[1028][1028], int i, int j){
+	ll ret = 0;
+	while(i > 0){
+		int x = j;
+		while(x > 0){
+			ret += b[i][x];
+			x -= (x & -x);
+		}
+		i -= (i & -i);
+	}
+	return ret;
+}
+void update(ll b[1028][1028], int i, int j, ll diff){
+	while(i < 1028){
+		int x = j;
+		while(x < 1028){
+			b[i][x] += diff;
+			x += (x & -x);
+		}
+		i += (i & -i);
+	}
+	return;
+}
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	cin >> n >> m;
+	for(int i = 1; i <= n; i++){
+		for(int j = 1; j <= n; j++){
+			cin >> a[i][j];
+			update(b, i, j, a[i][j]);
+		}
+	}
+	for(int i = 0; i < m; i++){
+		cin >> w;
+		if(w == 0){
+			cin >> x >> y >> c;
+			ll diff = c - a[x][y];
+			update(b, x, y, diff);
+			a[x][y] = c;
+		}else{
+			cin >> x1 >> y1 >> x2 >> y2;
+			cout << sum(b, x2, y2) - sum(b, x1 - 1, y2) - sum(b, x2, y1 - 1) + sum(b, x1 - 1, y1 - 1) << "\n";
+		}
+	}
 }
