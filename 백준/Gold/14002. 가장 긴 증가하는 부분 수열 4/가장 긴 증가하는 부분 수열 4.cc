e72b9b87037d2332faac4d01787c7ec27af654a0
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2024-02-10 12:56:26 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
@@ -1,31 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n, num, lis[1004], len, ret;
+int n, num, lis[1004], len;
 pair<int, int> a[1004];
-stack<int> st;
+stack<int> stk;
+
 int main(){
 	cin >> n;
-	fill(&lis[0], &lis[0] + 1004, 1004);
 	for(int i = 0; i < n; i++){
 		cin >> num;
-		auto lowerPos = lower_bound(lis, lis+len, num);
-		if(*lowerPos == 1004) len++;
+		auto lowerPos = lower_bound(lis, lis + len, num);
+		int pos = lowerPos - lis;
+		if(*lowerPos == 0) len++;
 		*lowerPos = num;
-		int pos = (int)(lowerPos - &lis[0]);
 		a[i].first = pos;
 		a[i].second = num;
 	}
 	cout << len << "\n";
-	for(int i = n-1; i >= 0; i--){
-		if(a[i].first == len-1){
-			st.push(a[i].second);
+	for(int i = n - 1; i >= 0; i--){
+		if(a[i].first == len - 1){
 			len--;
+			stk.push(a[i].second);
 		}
 	}
-	while(!st.empty()){
-		cout << st.top() << " ";
-		st.pop();
+	while(stk.size()){
+		cout << stk.top() << " ";
+		stk.pop();
 	}
-	return 0;
-	
 }
