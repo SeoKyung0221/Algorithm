790f7433b8125beb451d746e25fdec392e99a8f9
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2024-02-11 23:30:39 +0900

    [Silver III] Title: 큰 수 A+B (2), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/15353. 큰 수 A＋B （2）/큰 수 A＋B （2）.cc
+++ 백준/Silver/15353. 큰 수 A＋B （2）/큰 수 A＋B （2）.cc
@@ -1,10 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
-string a, b;
+string a, b, ret;
 
-string string_add(string a, string b){
+int main(){
+	cin >> a >> b;
+	
 	int sum = 0;
-	string ret = "";
 	while(a.size() || b.size() || sum){
 		if(a.size()){
 			sum += a.back() - '0';
@@ -14,13 +15,9 @@ string string_add(string a, string b){
 			sum += b.back() - '0';
 			b.pop_back();
 		}
-		ret += (sum % 10) + '0';
+		ret += sum % 10 + '0';
 		sum /= 10;
 	}
 	reverse(ret.begin(), ret.end());
-	return ret;
-}
-int main(){
-	cin >> a >> b;
-	cout << string_add(a, b) << "\n";
+	cout << ret << "\n";
 }
