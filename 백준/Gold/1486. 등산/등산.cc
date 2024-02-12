f9c0f2104cb0169ddfd082869bc8cdd5233e0f9a
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2024-02-13 02:19:29 +0900

    [Gold II] Title: 등산, Time: 304 ms, Memory: 37184 KB -BaekjoonHub

--- 백준/Gold/1486. 등산/등산.cc
+++ 백준/Gold/1486. 등산/등산.cc
@@ -1,38 +1,40 @@
 #include<bits/stdc++.h>
 using namespace std;
+int n, m, t, d, a[26][26], b[3000][3000], ret;
+string s;
 const int dy[] = {-1, 0, 1, 0};
 const int dx[] = {0, 1, 0, -1};
-const int INF = 1e9;
-int n, m, t, d, a[29][29], cost[2600][2600];
-string s;
 vector<int> v;
 
 int main(){
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	fill(&cost[0][0], &cost[0][0] + 2600 * 2600, INF);
 	cin >> n >> m >> t >> d;
 	for(int i = 0; i < n; i++){
 		cin >> s;
 		for(int j = 0; j < m; j++){
-			if(s[j] >= 'a' && s[j] <= 'z') a[i][j] = s[j] - 'a' + 26;
-			else a[i][j] = s[j] - 'A';
-			v.push_back(i * 100 + j);
+			if(s[j] >= 'A' && s[j] <= 'Z'){
+				a[i][j] = s[j] - 'A';
+			}else{
+				a[i][j] = s[j] - 'a' + 26;
+			}
 		}
 	}
 	for(int i = 0; i < n; i++){
 		for(int j = 0; j < m; j++){
-			for(int k = 0; k < 4; k++){
-				int ny = i + dy[k];
-				int nx = j + dx[k];
+			v.push_back(100 * i + j);
+		}
+	}
+	fill(&b[0][0], &b[0][0] + 3000 * 3000, 1e9);
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < m; j++){
+			for(int d = 0; d < 4; d++){
+				int ny = i + dy[d];
+				int nx = j + dx[d];
 				if(ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
-				int h = abs(a[ny][nx] - a[i][j]);
+				int h = abs(a[i][j] - a[ny][nx]);
 				if(h <= t){
-					if(a[ny][nx] > a[i][j]){
-						cost[i * 100 + j][ny * 100 + nx] = h * h;
-					}else{
-						cost[i * 100 + j][ny * 100 + nx] = 1;
+					if(a[i][j] >= a[ny][nx]) b[i * 100 + j][ny * 100 + nx] = 1;
+					else{
+						b[i * 100 + j][ny * 100 + nx] = h * h;
 					}
 				}
 			}
@@ -41,13 +43,18 @@ int main(){
 	for(int k : v){
 		for(int i : v){
 			for(int j : v){
-				cost[i][j] = min(cost[i][j], cost[i][k] + cost[k][j]);
+				if(b[i][j] > b[i][k] + b[k][j]){
+					b[i][j] = b[i][k] + b[k][j];
+				}
 			}
 		}
 	}
-	int ret = a[0][0];
+	ret = a[0][0];
+	
 	for(int i : v){
-		if(cost[0][i] + cost[i][0] <= d) ret = max(ret, a[i / 100][i % 100]);
+		if(b[0][i] + b[i][0] <= d){
+			ret = max(ret, a[i / 100][i % 100]);
+		}
 	}
 	cout << ret << "\n";
 }
