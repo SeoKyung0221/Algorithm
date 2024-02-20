f9d8be70218982a26bb1b3b95cb647d7b30fb685
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2024-02-20 19:01:43 +0900

    [Gold IV] Title: 뱀, Time: 0 ms, Memory: 2108 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.cc
+++ 백준/Gold/3190. 뱀/뱀.cc
@@ -1,30 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int dy[] = {0, 1, 0, -1};
-const int dx[] = {1, 0, -1, 0};
-int n, k, a[104][104], q, w, l, t, cnt, dir, y, x, visited[104][104], _time;
+const int dy[] = {-1, 0, 1, 0};
+const int dx[] = {0, 1, 0, -1};
+int n, k, a[104][104], visited[104][104], y, l, x, cnt, dir = 1, _time;
 char c;
 vector<pair<int, int>> v;
+deque<pair<int, int>> dq;
 
 int main(){
 	cin >> n >> k;
 	for(int i = 0; i < k; i++){
-		cin >> q >> w;
-		q--; w--;
-		a[q][w] = 1;
+		cin >> y >> x;
+		y--; x--;
+		a[y][x] = 1;
 	}
 	cin >> l;
 	for(int i = 0; i < l; i++){
-		cin >> t >> c;
-		if(c == 'D') v.push_back({t, 1});
-		else v.push_back({t, 3});
+		cin >> x >> c;
+		if(c == 'D') v.push_back({x, 1});
+		else v.push_back({x, 3});
 	}
-	deque<pair<int, int>> dq;
-	dq.push_back({0, 0});
 	visited[0][0] = 1;
-	while(dq.size()){
+	dq.push_back({0, 0});
+	
+	while(true){
 		_time++;
 		tie(y, x) = dq.front();
+		
 		int ny = y + dy[dir];
 		int nx = x + dx[dir];
 		if(ny < 0 || ny >= n || nx < 0 || nx >= n || visited[ny][nx]) break;
@@ -35,10 +37,10 @@ int main(){
 			visited[dq.back().first][dq.back().second] = 0;
 			dq.pop_back();
 		}
-		visited[ny][nx] = 1;
 		dq.push_front({ny, nx});
+		visited[ny][nx] = 1;
 		
-		if(v[cnt].first == _time){
+		if(_time == v[cnt].first){
 			dir = (dir + v[cnt].second) % 4;
 			cnt++;
 		}
