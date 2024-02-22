2a20ec818b55b9b84963991745ec31d83fe51dae
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2024-02-23 04:43:02 +0900

    [Platinum V] Title: 오민식의 고민, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Platinum/1219. 오민식의 고민/오민식의 고민.cc
+++ 백준/Platinum/1219. 오민식의 고민/오민식의 고민.cc
@@ -1,50 +1,51 @@
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-const ll INF = 1e18;
-int n, s, e, m, a, b, pay, visited[54];
-ll dist[54], cost[54];
-vector<pair<int,int>> adj[54];
+ll n, s, e, m, u, v, p, a[54], dist[54], visited[54];
+vector<pair<ll, ll>> adj[54];
+
 int main(){
-	fill(dist, dist+54, -INF);
 	cin >> n >> s >> e >> m;
 	for(int i = 0; i < m; i++){
-		cin >> a >> b >> pay;
-		adj[a].push_back({b,pay});
+		cin >> u >> v >> p;
+		adj[u].push_back({p, v});
 	}
 	for(int i = 0; i < n; i++){
-		cin >> cost[i];
+		cin >> a[i];
 	}
-	dist[s] = cost[s]; queue<int> q;
+	fill(dist, dist + 54, -1e18);
+	dist[s] = a[s];
+	queue<ll> q;
+	
 	for(int i = 0; i < n; i++){
 		for(int here = 0; here < n; here++){
-			for(auto it : adj[here]){
-				int next = it.first;
-				int next_dist = it.second;
-				if(dist[here] != -INF && dist[next] < dist[here] + cost[next] - next_dist){
-					if(i == n-1) q.push(here);
-					dist[next] = dist[here] + cost[next] - next_dist;
+			for(pair<ll, ll> to : adj[here]){
+				ll next = to.second;
+				ll next_dist = to.first;
+				if(dist[here] != -1e18 && dist[here] + a[next] - next_dist > dist[next]){
+					if(i == n - 1) q.push(here);
+					dist[next] = dist[here] + a[next] - next_dist;
 				}
 			}
 		}
 	}
 	int flag = 0;
 	while(q.size()){
-		int here = q.front(); q.pop();
-		for(auto it : adj[here]){
-			int next = it.first;
-			if(visited[next] == 1) continue;
-			if(next == e){
-				flag = 1;
-				break;
-			}
-			visited[next] = 1;
-			q.push(next);
+		ll here = q.front();
+		q.pop();
+		
+		visited[here] = 1;
+		
+		if(here == e){
+			flag = 1;
+			break;
 		}
-		if(flag) break;
+		for(pair<ll, ll> to : adj[here]){
+			if(visited[to.second]) continue;
+			q.push(to.second);
+		}
 	}
-	if(dist[e] == -INF) cout << "gg" << "\n";
-	else if(flag == 1) cout << "Gee" << "\n";
+	if(dist[e] == -1e18) cout << "gg\n";
+	else if(flag) cout << "Gee\n";
 	else cout << dist[e] << "\n";
-	return 0;
 }
