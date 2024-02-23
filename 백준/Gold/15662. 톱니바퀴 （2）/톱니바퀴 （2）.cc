37a0ca717e93033ebe298aa271023dda5e3cc228
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2024-02-23 15:49:07 +0900

    [Gold V] Title: 톱니바퀴 (2), Time: 0 ms, Memory: 2048 KB -BaekjoonHub

--- 백준/Gold/15662. 톱니바퀴 （2）/톱니바퀴 （2）.cc
+++ 백준/Gold/15662. 톱니바퀴 （2）/톱니바퀴 （2）.cc
@@ -1,57 +1,68 @@
 #include<bits/stdc++.h>
 using namespace std;
-int t, k, a, b, ret;
-string s[1004];
+int t, k, num, dir, ret;
+string s;
+vector<int> a[1004];
 
-void rotate_(int pos, int flag){
-	if(flag == 0) rotate(s[pos].begin(), s[pos].begin() + 1, s[pos].end());
-	else rotate(s[pos].rbegin(), s[pos].rbegin() + 1, s[pos].rend());
-}
-int findL(int pos){
-	for(int i = pos; i >= 1; i--){
-		if(s[i][6] == s[i-1][2]){
-			return i;
+void goLeft(int num, int dir){
+	int flag = (dir == 1) ? -1 : 1;
+	
+	if(num > 0){
+		if(a[num][6] != a[num - 1][2]){
+			goLeft(num - 1, flag);
 		}
 	}
-	return 0;
-}
-int findR(int pos){
-	for(int i = pos; i <= t-2; i++){
-		if(s[i][2] == s[i+1][6]){
-			return i;
-		}
-	}
-	return t-1;
+	
+	if(dir == 1){
+		rotate(a[num].rbegin(), a[num].rbegin() + 1, a[num].rend());
+	}else rotate(a[num].begin(), a[num].begin() + 1, a[num].end());
 }
 
+void goRight(int num, int dir){
+	int flag = (dir == 1) ? -1 : 1;
+	
+	if(num < t - 1){
+		if(a[num][2] != a[num + 1][6]){
+			goRight(num + 1, flag);
+		}
+	}
+	
+	if(dir == 1){
+		rotate(a[num].rbegin(), a[num].rbegin() + 1, a[num].rend());
+	}else rotate(a[num].begin(), a[num].begin() + 1, a[num].end());
+}
 int main(){
 	cin >> t;
 	for(int i = 0; i < t; i++){
-		cin >> s[i];
+		cin >> s;
+		for(int j = 0; j < s.size(); j++){
+			a[i].push_back(s[j] - '0');
+		}
 	}
 	cin >> k;
 	for(int i = 0; i < k; i++){
-		cin >> a >> b;
-		a--;
-		b = (b == -1) ? 0 : 1;
-		int l = findL(a);
-		int r = findR(a);
+		cin >> num >> dir;
+		num--;
 		
-		int cnt = 0;
-		for(int j = a; j >= l; j--){
-			if(cnt % 2 == 0) rotate_(j, b);
-			else rotate_(j, !b);
-			cnt++;
+		int flag = (dir == 1) ? -1 : 1;
+		if(num > 0){
+			if(a[num][6] != a[num - 1][2]){
+				goLeft(num - 1, flag);		
+			}
 		}
-		cnt = 1;
-		for(int j = a+1; j <= r; j++){
-			if(cnt % 2 == 0) rotate_(j, b);
-			else rotate_(j, !b);
-			cnt++;
+		
+		if(num < t - 1){
+			if(a[num][2] != a[num + 1][6]){
+				goRight(num + 1, flag);
+			}
 		}
+		
+		if(dir == 1){
+			rotate(a[num].rbegin(), a[num].rbegin() + 1, a[num].rend());
+		}else rotate(a[num].begin(), a[num].begin() + 1, a[num].end());
 	}
 	for(int i = 0; i < t; i++){
-		if(s[i][0] == '1') ret++;
+		if(a[i][0] == 1) ret++;
 	}
 	cout << ret << "\n";
 }
