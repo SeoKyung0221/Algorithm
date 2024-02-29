c15940a241e45f694fa8b73fa1b7f9b128e410f2
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2024-02-29 15:43:40 +0900

    [Silver II] Title: 연속합, Time: 24 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.cc
+++ 백준/Silver/1912. 연속합/연속합.cc
@@ -1,14 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n, num, sum, ret = -1e9;
+int n, a[100004], cnt, ret = -1e9;
 
 int main(){
 	cin >> n;
+	
 	for(int i = 0; i < n; i++){
-		cin >> num;
-		sum += num;
-		ret = max(ret, sum);
-		if(sum < 0) sum = 0;
+		cin >> a[i];
+		cnt += a[i];
+		ret = max(ret, cnt);
+		if(cnt < 0) cnt = 0;
 	}
-	cout << ret << '\n';
+	cout << ret << "\n";
 }
