38ed42a70d4c5d8de092f6bb54167d89869efea0
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2024-03-02 00:46:44 +0900

    [Gold II] Title: 미확인 도착지, Time: 112 ms, Memory: 3220 KB -BaekjoonHub

--- 백준/Gold/9370. 미확인 도착지/미확인 도착지.cc
+++ 백준/Gold/9370. 미확인 도착지/미확인 도착지.cc
@@ -2,29 +2,30 @@
 using namespace std;
 const int INF = 1e9;
 int te, n, m, t, s, g, h, a, b, d, x, dist[2004];
+priority_queue<pair<int, int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
 
 int main(){
 	cin >> te;
 	while(te--){
-		fill(dist, dist + 2004, INF);
 		vector<pair<int,int>> adj[2004];
 		vector<int> v;
-		cin >> n >> m >> t;
-		cin >> s >> g >> h;
-		for(int i = 0; i < m; i++){
-            cin >> a >> b >> d;
-            d *= 2;
-            if(a == g && b == h){
-                d--;
-            }else if(a == h && b == g)d--;
-            adj[a].push_back(make_pair(d, b));
-            adj[b].push_back(make_pair(d, a));
-        }
+		fill(dist, dist + 2004, INF);
+		cin >> n >> m >> t >> s >> g >> h;
+		for(int i =0; i < m; i++){
+			cin >> a >> b >> d;
+			d *= 2;
+			if((a == g && b == h) || (a == h && b == g)){
+				adj[a].push_back({d-1, b});
+				adj[b].push_back({d-1, a});
+			}else{
+				adj[a].push_back({d, b});
+				adj[b].push_back({d, a});
+			}
+		}
 		for(int i = 0; i < t; i++){
 			cin >> x;
 			v.push_back(x);
 		}
-		priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
 		dist[s] = 0;
 		pq.push({0, s});
 		while(pq.size()){
@@ -32,9 +33,9 @@ int main(){
 			int here_dist = pq.top().first;
 			pq.pop();
 			if(dist[here] != here_dist) continue;
-			for(auto j : adj[here]){
-				int next = j.second;
-				int next_dist = j.first;
+			for(auto i : adj[here]){
+				int next = i.second;
+				int next_dist = i.first;
 				if(dist[next] > dist[here] + next_dist){
 					dist[next] = dist[here] + next_dist;
 					pq.push({dist[next], next});
@@ -43,7 +44,9 @@ int main(){
 		}
 		sort(v.begin(), v.end());
 		for(int i : v){
-			if(dist[i] % 2 != 0 && dist[i] != INF) cout << i << " ";
+			if(dist[i] != INF && dist[i] % 2 != 0){
+				cout << i << " ";
+			}
 		}
 		cout << "\n";
 	}
