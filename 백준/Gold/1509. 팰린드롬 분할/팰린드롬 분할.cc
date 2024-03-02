e88bbeb9a094718151b4b018bff4d6ee2c5c2821
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2024-03-02 16:20:32 +0900

    [Gold I] Title: 팰린드롬 분할, Time: 68 ms, Memory: 26528 KB -BaekjoonHub

--- 백준/Gold/1509. 팰린드롬 분할/팰린드롬 분할.cc
+++ 백준/Gold/1509. 팰린드롬 분할/팰린드롬 분할.cc
@@ -1,34 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
 string s;
-int dp[2504][2504], dp2[2504];
+int n, dp[2504][2504], dp2[2504];
 
-int solve(int idx){
-	if(idx == s.size()) return 0;
+int go(int here){
+	if(here == n) return 0;
 	
-	int &ret = dp2[idx];
+	int &ret = dp2[here];
 	if(ret != 1e9) return ret;
 	
-	for(int i = 1; i <= s.size(); i++){
-		if(dp[idx][i]) ret = min(ret, solve(idx + i) + 1);
+	for(int size = 1; size + here - 1 < n; size++){
+		if(dp[here][size]) ret = min(ret, go(here + size) + 1);
 	}
 	return ret;
-}
+} 
 int main(){
-	fill(dp2, dp2 + 2504, 1e9);
 	cin >> s;
-	for(int i = 0; i < s.size(); i++){
+	n = s.size();
+	
+	for(int i = 0; i < n; i++){
 		dp[i][1] = 1;
 	}
-	for(int i = 0; i < s.size()-1; i++){
-		if(s[i] == s[i+1]) dp[i][2] = 1;
+	for(int i = 0; i < n - 1; i++){
+		if(s[i] == s[i + 1]) dp[i][2] = 1;
 	}
-	for(int _size = 3; _size <= s.size(); _size++){
-		for(int i = 0; i + _size <= s.size(); i++){
-			if(s[i] == s[i+_size-1] && dp[i+1][_size-2]){
-				dp[i][_size] = 1;
-			}
+	for(int size = 3; size <= n; size++){
+		for(int i = 0; i + size - 1 < n; i++){
+			if(s[i] == s[i + size - 1] && dp[i + 1][size - 2]) dp[i][size] = 1;
 		}
 	}
-	cout << solve(0) << "\n";
+	fill(dp2, dp2 + 2504, 1e9);
+	cout << go(0) << "\n";
+	return 0;
 }
