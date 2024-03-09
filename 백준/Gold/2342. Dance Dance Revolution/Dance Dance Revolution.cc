228e5decf5d699c870a2bd9e367e20053e19b5d9
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2024-03-09 19:17:04 +0900

    [Gold III] Title: Dance Dance Revolution, Time: 40 ms, Memory: 19996 KB -BaekjoonHub

--- 백준/Gold/2342. Dance Dance Revolution/Dance Dance Revolution.cc
+++ 백준/Gold/2342. Dance Dance Revolution/Dance Dance Revolution.cc
@@ -1,35 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n, num, dp[5][5][100004];
+int num, dp[100004][5][5];
 vector<int> v;
 
-int check(int here, int next){
-	if(here == 0) return 2;
-	else if(here == next) return 1;
-	else if(abs(here - next) == 2) return 4;
+int move(int a, int b){
+	
+	if(a == 0) return 2;
+	else if(a == b) return 1;
+	else if(abs(a - b) == 2) return 4;
 	else return 3;
 }
-int solve(int l, int r, int cnt){
-	if(cnt == n) return 0;
+int go(int idx, int left, int right){
+	if(idx == v.size()) return 0;
 	
-	int &ret = dp[l][r][cnt];
-	if(ret != -1) return ret;
+	int &ret = dp[idx][left][right];
+	if(ret != 1e9) return ret;
 	
-	int left = solve(v[cnt], r, cnt+1) + check(l, v[cnt]);
-	int right = solve(l, v[cnt], cnt+1) + check(r, v[cnt]);
+	ret = min(ret, go(idx + 1, v[idx], right) + move(left, v[idx]));
+	ret = min(ret, go(idx + 1, left, v[idx]) + move(right, v[idx]));
 	
-	ret = min(left, right);
-	
-	return ret;
+	return ret; 
 }
 int main(){
-	memset(dp, -1, sizeof(dp));
 	while(true){
 		cin >> num;
 		if(num == 0) break;
 		v.push_back(num);
 	}
-	n = v.size();
-	cout << solve(0, 0, 0) << "\n";	
-	return 0;
+	fill(&dp[0][0][0], &dp[0][0][0] + 100004 * 5 * 5, 1e9);
+	cout << go(0, 0, 0) << "\n";
 }
