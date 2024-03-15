82549bdd26eeea7141769056db5e28b6406387f3
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2024-03-15 23:03:34 +0900

    [Gold III] Title: 경사로, Time: 0 ms, Memory: 2064 KB -BaekjoonHub

--- 백준/Gold/14890. 경사로/경사로.cc
+++ 백준/Gold/14890. 경사로/경사로.cc
@@ -1,18 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n, l, a[104][104], b[104][104], ret;
+int n, l, a[104][104], ret;
 
-void solve(int a[104][104]){
+void solve(){
 	for(int i = 0; i < n; i++){
 		int cnt = 1;
 		int j;
 		for(j = 0; j < n - 1; j++){
-			if(a[i][j] == a[i][j+1]) cnt++;
-			else if(a[i][j] + 1 == a[i][j+1] && cnt >= l) cnt = 1;
-			else if(a[i][j] - 1 == a[i][j+1] && cnt >= 0) cnt = -l + 1;
-			else break;
+			if(a[i][j] == a[i][j + 1]) cnt++;
+			else if(a[i][j] == a[i][j + 1] - 1 && cnt >= l){
+				cnt = 1;
+			}else if(a[i][j] == a[i][j + 1] + 1 && cnt >= 0){
+				cnt = -l + 1;
+			}else break;
 		}
-		if(j == n-1 && cnt >= 0) ret++;
+		if(j == n - 1 && cnt >= 0) ret++;
+	}
+	
+	for(int j = 0; j < n; j++){
+		int cnt = 1;
+		int i;
+		for(i = 0; i < n - 1; i++){
+			if(a[i][j] == a[i + 1][j]) cnt++;
+			else if(a[i][j] == a[i + 1][j] - 1 && cnt >= l){
+				cnt = 1;
+			}else if(a[i][j] == a[i + 1][j] + 1 && cnt >= 0){
+				cnt = -l + 1;
+			}else break;
+		}
+		if(i == n - 1 && cnt >= 0) ret++;
 	}
 	return;
 }
@@ -21,11 +37,9 @@ int main(){
 	for(int i = 0; i < n; i++){
 		for(int j = 0; j < n; j++){
 			cin >> a[i][j];
-			b[j][i] = a[i][j];
 		}
 	}
-	solve(a);
-	solve(b);
+	solve();
 	
 	cout << ret << "\n";
 }
