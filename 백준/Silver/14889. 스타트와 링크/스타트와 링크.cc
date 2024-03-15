aac8822061322b88421de668ea7a8524ce91cafc
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2024-03-15 22:33:48 +0900

    [Silver I] Title: 스타트와 링크, Time: 112 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.cc
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.cc
@@ -1,18 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n, a[24][24], ret = 1e9;
+int n, a[21][21], ret = 1e9;
 
-int go(vector<int> &start, vector<int> &link){
-	int s1 = 0, s2 = 0;
+int solve(vector<int> &start, vector<int> &link){
+	int ans1 = 0, ans2 = 0;
+	
 	for(int i = 0; i < n / 2; i++){
 		for(int j = 0; j < n / 2; j++){
 			if(i == j) continue;
-			s1 += a[start[i]][start[j]];
-			s2 += a[link[i]][link[j]];
+			ans1 += a[start[i]][start[j]];
+			ans2 += a[link[i]][link[j]];
 		}
 	}
-	return abs(s1 - s2);
+	return abs(ans1 - ans2);
 }
+
 int main(){
 	cin >> n;
 	for(int i = 1; i <= n; i++){
@@ -20,14 +22,15 @@ int main(){
 			cin >> a[i][j];
 		}
 	}
-	for(int i = 0; i < (1 << n); i++){
+	for(int i = 1; i < (1 << n) - 1; i++){
+		if(__builtin_popcount(i) != n / 2) continue; 
 		vector<int> start, link;
-		if(__builtin_popcount(i) != n / 2) continue;
 		for(int j = 0; j < n; j++){
-			if(i & (1 << j)) start.push_back(j + 1);
-			else link.push_back(j + 1);
+			if(i & (1 << j)){
+				start.push_back(j + 1);
+			}else link.push_back(j + 1);
 		}
-		ret = min(ret, go(start, link));
+		ret = min(ret, solve(start, link));
 	}
 	cout << ret << "\n";
 }
