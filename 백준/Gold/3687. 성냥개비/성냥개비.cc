eacd5a91a6a22f3d62b7c707269cbf1004031fce
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2024-03-18 07:09:35 +0900

    [Gold II] Title: 성냥개비, Time: 4 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/3687. 성냥개비/성냥개비.cc
+++ 백준/Gold/3687. 성냥개비/성냥개비.cc
@@ -4,14 +4,14 @@ int te, n, a[10] = {6, 2, 5, 5, 4, 5, 6, 3, 7, 6};
 string max_ret, dp[104];
 const string INF = "1111111111111111111111111111111111111111111111111112";
 
-void make_max(int n){
-	if(n & 1){
+void make_max(int k){
+	if(k & 1){
 		max_ret += "7";
-		n -= 3;
+		k -= 3;
 	}
-	while(n){
+	while(k){
 		max_ret += "1";
-		n -= 2;
+		k -= 2;
 	}
 	return;
 }
@@ -20,32 +20,27 @@ bool check(string a, string b){
 	if(a.size() == b.size()) return a > b;
 	else return a.size() > b.size();
 }
-string make_min(int idx){
-	if(idx == 0) return "";
+string go(int idx){
+	if(idx == 0 && idx != 1) return "";
 	
 	string &ret = dp[idx];
 	if(ret != INF) return ret;
 	
 	for(int i = 0; i <= 9; i++){
 		if(idx - a[i] == 0 && i == 0) continue;
-		if(idx - a[i] >= 0 && idx - a[i] != 1){
-			if(check(ret, make_min(idx - a[i]) + to_string(i))){
-				ret = make_min(idx - a[i]) + to_string(i);
-			}
-		}
+		if(idx - a[i] == 1) continue;
+		if(idx - a[i] >= 0 && check(ret, go(idx - a[i]) + to_string(i))) ret = go(idx - a[i]) + to_string(i);
 	}
 	return ret;
 }
 int main(){
 	cin >> te;
 	while(te--){
+		cin >> n;
 		max_ret = "";
 		fill(dp, dp + 104, INF);
 		
-		cin >> n;
-		
 		make_max(n);
-		
-		cout << make_min(n) << " " << max_ret << "\n";
+		cout << go(n) << " " << max_ret << "\n";
 	}
 }
