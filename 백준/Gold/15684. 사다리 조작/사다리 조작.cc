780d3b0c941a6ce110ae7c28798885de0c36930f
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2024-03-18 18:58:59 +0900

    [Gold III] Title: 사다리 조작, Time: 1484 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/15684. 사다리 조작/사다리 조작.cc
+++ 백준/Gold/15684. 사다리 조작/사다리 조작.cc
@@ -1,29 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n, m, h, a, b, ret = 1e9, visited[34][14];
+int n, m, h, visited[34][14], a, b, ret = 1e9;
 
 bool check(){
 	for(int i = 1; i <= n; i++){
 		int start = i;
 		for(int j = 1; j <= h; j++){
-			if(visited[j][start-1]){
-				start--;
-			}else if(visited[j][start]){
-				start++;
-			}
+			if(visited[j][start-1]) start--;
+			else if(visited[j][start]) start++;
 		}
 		if(start != i) return false;
 	}
 	return true;
 }
 void go(int idx, int cnt){
-	if(cnt > 3 || cnt >= ret) return;
-	
+	if(cnt > 3 || cnt >= ret){
+		return;
+	}
 	if(check()){
 		ret = min(ret, cnt);
 		return;
 	}
-	
 	for(int i = idx; i <= h; i++){
 		for(int j = 1; j <= n; j++){
 			if(visited[i][j-1] || visited[i][j] || visited[i][j+1]) continue;
@@ -39,6 +36,7 @@ int main(){
 		cin >> a >> b;
 		visited[a][b] = 1;
 	}
+	
 	go(1, 0);
 	
 	cout << ((ret == 1e9) ? -1 : ret) << "\n";
