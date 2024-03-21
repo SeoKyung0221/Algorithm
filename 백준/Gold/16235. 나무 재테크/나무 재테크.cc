fa769443a5b5a3fee15ccf24d10094849a696358
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2024-03-22 03:39:51 +0900

    [Gold III] Title: 나무 재테크, Time: 140 ms, Memory: 2292 KB -BaekjoonHub

--- 백준/Gold/16235. 나무 재테크/나무 재테크.cc
+++ 백준/Gold/16235. 나무 재테크/나무 재테크.cc
@@ -1,27 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n, m, k, a[14][14], c[14][14], x, y, z, ret;
-vector<int> b[14][14];
+int n, m, k, z, y, x, a[11][11], c[11][11], ret;
 const int dy[] = {-1, -1, -1, 0, 1, 1, 1, 0};
 const int dx[] = {-1, 0, 1, 1, 1, 0, -1, -1};
+vector<int> b[11][11];
 
 void springSummer(){
 	for(int i = 0; i < n; i++){
 		for(int j = 0; j < n; j++){
 			if(b[i][j].size() == 0) continue;
-			vector<int> v;
 			int temp = 0;
+			vector<int> v;
 			sort(b[i][j].begin(), b[i][j].end());
-			for(int h : b[i][j]){
-				if(c[i][j] >= h){
-					c[i][j] -= h;
-					v.push_back(h + 1);
+			for(int k : b[i][j]){
+				if(c[i][j] >= k){
+					v.push_back(k + 1);
+					c[i][j] -= k;
 				}else{
-					temp += h / 2;
+					temp += k / 2;
 				}
 			}
-			b[i][j] = v;
 			c[i][j] += temp;
+			b[i][j] = v;
 		}
 	}
 }
@@ -29,11 +29,11 @@ void fall(){
 	for(int i = 0; i < n; i++){
 		for(int j = 0; j < n; j++){
 			if(b[i][j].size() == 0) continue;
-			for(int p : b[i][j]){
-				if(p % 5 == 0){
-					for(int h = 0; h < 8; h++){
-						int ny = i + dy[h];
-						int nx = j + dx[h];
+			for(int k : b[i][j]){
+				if(k % 5 == 0){
+					for(int d = 0; d < 8; d++){
+						int ny = i + dy[d];
+						int nx = j + dx[d];
 						if(ny < 0 || ny >= n || nx < 0 || nx >= n) continue;
 						b[ny][nx].push_back(1);
 					}
@@ -42,7 +42,6 @@ void fall(){
 		}
 	}
 }
-
 void winter(){
 	for(int i = 0; i < n; i++){
 		for(int j = 0; j < n; j++){
@@ -51,13 +50,13 @@ void winter(){
 	}
 }
 int main(){
-	fill(&c[0][0], &c[0][0] + 14 * 14, 5);
 	cin >> n >> m >> k;
 	for(int i = 0; i < n; i++){
 		for(int j = 0; j < n; j++){
 			cin >> a[i][j];
 		}
 	}
+	fill(&c[0][0], &c[0][0] + 11 * 11, 5);
 	for(int i = 0; i < m; i++){
 		cin >> x >> y >> z;
 		x--; y--;
@@ -70,9 +69,9 @@ int main(){
 	}
 	for(int i = 0; i < n; i++){
 		for(int j = 0; j < n; j++){
-			if(b[i][j].size() == 0) continue;
 			ret += b[i][j].size();
 		}
 	}
 	cout << ret << "\n";
+	return 0;
 }
