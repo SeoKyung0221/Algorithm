938299351fc13e7080e53c62ef03fc4f89535ef5
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2024-05-10 19:51:52 +0900

    [Bronze I] Title: Best Chance, Time: 112 ms, Memory: 7564 KB -BaekjoonHub

--- 백준/Bronze/31800. Best Chance/Best Chance.cc
+++ 백준/Bronze/31800. Best Chance/Best Chance.cc
@@ -3,6 +3,8 @@ using namespace std;
 int n, a[300004], b[300004], mx;
 vector<int> v;
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
 	cin >> n;
 	for(int i = 0; i < n; i++){
 		cin >> a[i];
