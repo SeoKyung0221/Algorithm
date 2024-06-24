2732c1f194526b91580d5d5cdc3c9ab117cc4105
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2024-06-25 03:28:17 +0900

    [Gold IV] Title: 치즈, Time: 0 ms, Memory: 2108 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.cc
+++ 백준/Gold/2636. 치즈/치즈.cc
@@ -2,56 +2,64 @@
 using namespace std;
 const int dy[] = {-1, 0, 1, 0};
 const int dx[] = {0, 1, 0, -1};
-int n, m, a[104][104], visited[104][104];
-vector<pair<int, int>> v;
+int n, m, a[104][104], visited[104][104], ret, prev_ret;
 
-void go(int y, int x){
-	visited[y][x] = 1;
-	if(a[y][x] == 1){
-		v.push_back({y, x});
-		return;
+void bfs(){
+	memset(visited, 0, sizeof(visited));
+	queue<pair<int, int>> q;
+	q.push({0, 0});
+	visited[0][0] = 1;
+	
+	while(q.size()){
+		int y, x;
+		tie(y, x) = q.front();
+		q.pop();
+		for(int i = 0; i < 4; i++){
+			int ny = y + dy[i];
+			int nx = x + dx[i];
+			if(ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
+			if(visited[ny][nx] == 1) continue;
+			if(a[ny][nx] == 0){
+				visited[ny][nx] = 1;
+				q.push({ny, nx});
+			}else if(a[ny][nx] == 1){
+				visited[ny][nx] = 1;
+				a[ny][nx] = 0;
+			}
+		}
 	}
-	for(int i = 0; i < 4; i++){
-		int ny = y + dy[i];
-		int nx = x + dx[i];
-		if(ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
-		if(visited[ny][nx]) continue;
-		go(ny, nx);
+	return;
+}
+bool check(){
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < m; j++){
+			if(a[i][j] == 1) return false;
+		}
 	}
+	return true;
+}
+int cheeze(){
+	int ret = 0;
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < m; j++){
+			if(a[i][j] == 1) ret++;
+		}
+	}
+	return ret;
 }
 int main(){
 	cin >> n >> m;
 	for(int i = 0; i < n; i++){
 		for(int j = 0; j < m; j++){
 			cin >> a[i][j];
+			if(a[i][j] == 1) prev_ret += 1;
 		}
 	}
-	int cnt = 0;
-	int cnt3 = 0;
 	while(true){
-		memset(visited, 0, sizeof(visited));
-		v.clear();
-		int cnt2 = 0;
-		int flag = 1;
-		cnt++;
-		for(int i = 0; i < n; i++){
-			for(int j = 0; j < m; j++){
-				if(a[i][j] == 1) cnt2++;
-			}
-		}
-		go(0, 0);
-		for(auto i : v){
-			a[i.first][i.second] = 0;
-		}
-		for(int i = 0; i < n; i++){
-			for(int j = 0; j < m; j++){
-				if(a[i][j] == 1) flag = 0;
-			}
-		}
-		if(flag){
-			cnt3 = cnt2;
-			break;
-		}
+		ret++;
+		bfs();
+		if(check()) break;
+		prev_ret = cheeze();
 	}
-	cout << cnt << "\n" << cnt3 << "\n";
+	cout << ret << "\n" << prev_ret << "\n";
 }
