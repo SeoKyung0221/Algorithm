340045033fba0bc94f607992d97d5b09a444c908
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2024-06-25 23:31:51 +0900

    [Gold III] Title: 괄호 추가하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.cc
+++ 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.cc
@@ -2,35 +2,37 @@
 using namespace std;
 int n, ret = -1e9;
 string s;
-vector<char> oper_;
-vector<int> num_;
+vector<int> num;
+vector<char> oper_str;
 
-int oper(int a, int b, char c){
-	if(c == '+') return a + b;
-	if(c == '-') return a - b;
-	if(c == '*') return a * b;
+int oper(char a, int b, int c){
+	if(a == '+') return b + c;
+	if(a == '-') return b - c;
+	if(a == '*') return b * c;
 }
-void go(int idx, int num){
-	if(idx == num_.size() - 1){
-		ret = max(ret, num);
+
+void go(int idx, int _num){
+	if(idx == num.size() - 1){
+		ret = max(ret, _num);
 		return;
 	}
-	go(idx + 1, oper(num, num_[idx + 1], oper_[idx]));
+	go(idx + 1, oper(oper_str[idx], _num, num[idx + 1]));
 	
-	if(idx + 2 <= num_.size() - 1){
-		int temp = oper(num_[idx + 1], num_[idx + 2], oper_[idx+1]);
-		go(idx + 2, oper(num, temp, oper_[idx]));
+	if(idx + 2 <= num.size() - 1){
+		int temp = oper(oper_str[idx + 1], num[idx + 1], num[idx + 2]);
+		
+		go(idx + 2, oper(oper_str[idx], _num, temp));
 	}
 	return;
 }
 int main(){
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
 	cin >> n >> s;
-	for(int i = 0; i < n; i++){
-		if(i % 2 == 0) num_.push_back(s[i] - '0');
-		else oper_.push_back(s[i]);
+	
+	for(int i = 0; i < s.size(); i++){
+		if(s[i] >= '0' && s[i] <= '9') num.push_back(s[i] - '0');
+		else oper_str.push_back(s[i]);
 	}
-	go(0, num_[0]);
-	cout << ret << '\n';
+	go(0, num[0]);
+	
+	cout << ret << "\n";
 }
