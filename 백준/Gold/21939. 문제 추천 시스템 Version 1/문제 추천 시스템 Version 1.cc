3a9921b32e286e9c05278b555b86a024c744a1eb
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2024-07-10 20:12:01 +0900

    [Gold IV] Title: 문제 추천 시스템 Version 1, Time: 308 ms, Memory: 11400 KB -BaekjoonHub

---
+++ 백준/Gold/21939. 문제 추천 시스템 Version 1/문제 추천 시스템 Version 1.cc
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n, p, l, m, x;
+string s;
+
+set<pair<int, int>> st;
+map<int, int> mp;
+
+int main(){
+	cin >> n;
+	for(int i = 0; i < n; i++){
+		cin >> p >> l;
+		
+		mp[p] = l;
+		st.insert({l, p});
+	}
+
+	cin >> m;
+	for(int i = 0; i < m; i++){
+		cin >> s;
+		if(s == "recommend"){
+			cin >> x;
+			if(x == 1){
+				auto it = st.rbegin();
+				cout << (*it).second << "\n";
+			}else if(x == -1){
+				auto it = st.begin();
+				cout << (*it).second << "\n";
+			}
+		}else if(s == "add"){
+			cin >> p >> l;
+			st.insert({l, p});
+			mp[p] = l;
+		}else if(s == "solved"){
+			cin >> p;
+			
+			st.erase({mp[p], p});
+			mp[p] = 0;
+		}
+	}
+}
