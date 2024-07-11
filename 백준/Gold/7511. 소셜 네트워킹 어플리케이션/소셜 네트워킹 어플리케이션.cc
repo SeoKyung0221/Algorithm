f8ec3560d518cf3be093ca5c2d7160a70725bf55
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2024-07-11 18:36:24 +0900

    [Gold V] Title: 소셜 네트워킹 어플리케이션, Time: 108 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Gold/7511. 소셜 네트워킹 어플리케이션/소셜 네트워킹 어플리케이션.cc
+++ 백준/Gold/7511. 소셜 네트워킹 어플리케이션/소셜 네트워킹 어플리케이션.cc
@@ -7,40 +7,40 @@ int Find(int x) {
     else return parent[x] = Find(parent[x]);
 }
 
-void Union(int x, int y) {
-    x = Find(x);
-    y = Find(y);
-
-    if (x == y) return;
-
-    if (x < y) parent[y] = x;
-    else parent[x] = y;
+void Union(int x, int y){
+	x = Find(x);
+	y = Find(y);
+	
+	if(x == y) return;
+	
+	if(x < y) parent[y] = x;
+	else parent[x] = y;
+	
+	return;
 }
-
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-
-    cin >> te;
-    for (int t = 1; t <= te; t++) {
-        cin >> n >> k;
-        for (int i = 0; i < n; i++) {
-            parent[i] = i;
-        }
-
-        for (int i = 0; i < k; i++) {
-            cin >> a >> b;
-            Union(a, b);
-        }
-
-        cout << "Scenario " << t << ":\n";
-        cin >> m;
-        for (int i = 0; i < m; i++) {
-            cin >> a >> b;
-            if (Find(a) == Find(b)) cout << "1\n"; // 최상위 부모 비교
-            else cout << "0\n";
-        }
-        cout << "\n";
-    }
-    return 0;
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	cin >> te;
+	for(int t = 1; t <= te; t++){
+		cin >> n >> k;
+		for(int i = 0; i < n; i++){
+			parent[i] = i;
+		}
+		
+		for(int i = 0; i < k; i++){
+			cin >> a >> b;
+			Union(a, b);
+		}
+		
+		cout << "Scenario " << t << ":\n";
+		cin >> m;
+		for(int i = 0; i < m; i++){
+			cin >> a >> b;
+			if(Find(a) == Find(b)) cout << "1\n";
+			else cout << "0\n";
+		}
+		cout << "\n";
+	}
 }
